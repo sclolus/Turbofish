d0831ce00647f8acdd634c77240702d2201be918
MordakLaVache <smartrabbitbeta>
2018-08-28 23:00:47 +0200

    [DEV) Success mixing of ASM and C code

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -2,6 +2,9 @@
 extern void scrollup(unsigned int);
 extern void print(char *);
 
+extern void asm_print(void);
+extern void asm_print_2(char *);
+
 extern kY;
 extern kattr;
 
@@ -9,6 +12,11 @@ void _start(void)
 {
 	kY = 18;
 	kattr = 0x5E;
+
+	asm_print_2("Les sangliers sont partis\n");
+
+	asm_print();
+
 	print("un message\n");
 
 	kattr = 0x4E;
