cc4a11b98eb2e2242ee6eaa72e41faa2226dec42
Mordak <mickael@boule.localdomain>
2018-09-17 17:45:51 +0200

    [DEV MINOR] Separate files IRQ of CPU and IRQ of PIC

--- kernel/src/system/irq_cpu.asm
+++ kernel/src/system/irq_cpu.asm
@@ -57,13 +57,6 @@ GLOBAL asm_simd_fpu_fp_exception
 GLOBAL asm_virtualize_exception
 GLOBAL asm_security_exception
 
-
-GLOBAL asm_default_interrupt
-GLOBAL asm_default_pic_master_interrupt
-GLOBAL asm_default_pic_slave_interrupt
-GLOBAL asm_clock_handler
-GLOBAL asm_keyboard_handler
-
 %macro PUSH_ALL_REGISTERS_WITH_ERRCODE_OFFSET 0
     pushad                ; EAX, ECX, EDX, EBX, and ESP, EBP, ESI, EDI
     push dword [ebp + 16] ; eflags
@@ -266,51 +259,4 @@ asm_page_fault:
     pop ebp
     ; bypass the error code
     add esp, 4
-    iret
-
-asm_default_interrupt:
-    iret
-
-asm_default_pic_master_interrupt:
-    mov al, 0x20
-    out 0x20, al
-    iret
-
-asm_default_pic_slave_interrupt:
-; IRQ8 is managed by master and slave, so we must inform the two PICS
-    mov al, 0x20
-    out 0x20, al
-    mov al, 0xA0
-    out 0xA0, al
-    iret
-
-extern putstr
-asm_clock_handler:
-	push eax
-    mov al, 0x20
-    out 0x20, al
-	pop eax
-    iret
-
-extern process_keyboard
-
-; 8042 chipset
-; 60h read or transmit data
-; 64h compute status or emmit command
-asm_keyboard_handler:
-    in al, 0x64
-    mov edx, eax
-    and edx, 0x1
-    cmp edx, 0
-    je asm_keyboard_handler ; wait after kerboard buffer is full
-
-    xor eax, eax
-    in al, 0x60 ; read scan_code
-
-    push eax
-    call process_keyboard
-    add esp, 4
-
-    mov al, 0x20
-    out 0x20, al
     iret
---
+++ kernel/src/system/irq_pic.asm
@@ -0,0 +1,54 @@
+[BITS 32]
+segment .text
+GLOBAL asm_default_interrupt
+GLOBAL asm_default_pic_master_interrupt
+GLOBAL asm_default_pic_slave_interrupt
+GLOBAL asm_clock_handler
+GLOBAL asm_keyboard_handler
+
+asm_default_interrupt:
+    iret
+
+asm_default_pic_master_interrupt:
+    mov al, 0x20
+    out 0x20, al
+    iret
+
+asm_default_pic_slave_interrupt:
+; IRQ8 is managed by master and slave, so we must inform the two PICS
+    mov al, 0x20
+    out 0x20, al
+    mov al, 0xA0
+    out 0xA0, al
+    iret
+
+extern putstr
+asm_clock_handler:
+	push eax
+    mov al, 0x20
+    out 0x20, al
+	pop eax
+    iret
+
+extern process_keyboard
+
+; 8042 chipset
+; 60h read or transmit data
+; 64h compute status or emmit command
+asm_keyboard_handler:
+    in al, 0x64
+    mov edx, eax
+    and edx, 0x1
+    cmp edx, 0
+    je asm_keyboard_handler ; wait after kerboard buffer is full
+
+    xor eax, eax
+    in al, 0x60 ; read scan_code
+
+    push eax
+    call process_keyboard
+    add esp, 4
+
+    mov al, 0x20
+    out 0x20, al
+    iret
