6a894f18d372f57a3c5a2fd3b225d8494c7067ca
Mordak <bmickael@student.42.fr>
2019-01-11 02:36:48 +0000

    Fix Value For CS segment

--- nucleus/src/asm/gdt.asm
+++ nucleus/src/asm/gdt.asm
@@ -1,4 +1,6 @@
-[BITS 32]	
+[BITS 32]
+
+; ACCESS BYTES DESCRIPTION
 %DEFINE ACCESSED 1; WAS IT ACCESSED
 %DEFINE READ_WRITE 1 << 1; FOR DATA SEGMENT IS WRITING ALLOWED ?
 %DEFINE GROWTH_DIRECTION 1 << 2; FOR DATA SEGMENT: TO LOWER OR TO HIGHER ADDRESS. FOR TEXT SEGMENT : CAN IT BE EXECTUDED WITH HIGHER PRIVILEDGE ?
@@ -7,11 +9,12 @@
 %DEFINE DPL 1 << 5 | 1 << 6; DESCRIPTOR PRIVILEGE LEVEL (RING)
 %DEFINE PR 1 << 7; PRESENT IN MEMORY RIGHT NOW ?
 
-%DEFINE LIMIT_16_19 1 | 1 << 1 | 1 << 2 | 1 << 3
+; LIMIT FLAG DESCRIPTION
 %DEFINE V 1 << 4; AVAILABLE TO USE FOR SYSTEM SOFTWARE ?
 %DEFINE LONGMODE 1 << 5; IS IT A 64 BIT MODE SEGMENT ?
 %DEFINE SIZE 1 << 6; (0) 16 BIT (1) FOR 32 BIT PROTECTED
 %DEFINE GRANULARITY 1 << 7; LIMIT IS IN 0 = BYTES, 1 = PAGES OF LIMIT 4096 BYTES EACH
+
 ;struc gdt_entry_struct
 ;	limit_0_15:				resb 2
 ;	base_0_15:				resb 2
@@ -20,6 +23,7 @@
 ;	limit_flags:			resb 1
 ;	base_24_31:				resb 1
 ;endstruc
+
 segment .data
 
 gdt_info:
@@ -80,22 +84,16 @@ init_gdt:
 	lgdt [gdt_info]
 
 	; CS IS CODE SEGMENT REGISTER
-;	mov ax, 0x8
-;	mov cs, ax
-
-;   jmp landing:0x8
-	jmp landing
+	jmp 0x8:landing
 landing:
 
-	; ES IS DATA SEGMENT REGISTER
-	; DS IS DATA SEGMENT REGISTER
-	; FS IS DATA SEGMENT REGISTER
-	; GS IS DATA SEGMENT REGISTER
+	; DS, ES, FS and GS ARE DATA SEGMENT REGISTER
 	mov ax, 0x10
 	mov ds, ax
 	mov es, ax
 	mov fs, ax
 	mov gs, ax
+
 	; SS IS STACK SEGMENT REGISTER
 	; mov ax, 0x18
 	; mov ss, ax
