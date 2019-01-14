147295a1210c1792286eca092891611a583ad457
vcombey <vcombey@student.42.fr>
2019-01-14 21:26:57 +0100

    pass int pointer and struct from rust to c works

--- sandbox/rust_to_c_and_asm/src/c/main.c
+++ sandbox/rust_to_c_and_asm/src/c/main.c
@@ -1,5 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
 extern int rust_jump(int i);
 
 int main(void) {
-	return rust_jump(42);
+	rust_jump(42);
+
+//	exit(1);
+	return 0;
+}
+
+struct mastruc {
+	int a;
+	int b;
+};
+
+int	c_jump(int a, int *ptr, struct mastruc m) {
+	printf("first param int : '%d'\nsecond param ptr:  '%p' , '%d'\nthird param struct:'%d', '%d'\n",a, ptr, *ptr, m.a, m.b);  
+	return 3;
 }
--- sandbox/rust_to_c_and_asm/src/lib.rs
+++ sandbox/rust_to_c_and_asm/src/lib.rs
@@ -1,7 +1,20 @@
-#![feature(lang_items)]
-#![feature(format_args_nl)]
+
+#[repr(C)]
+struct Mastruct {
+    lala: u32,
+    lolo: u32,
+}
+
+extern {
+    fn c_jump(a: u32, ptr: *const  u32, mastruc: Mastruct) -> u32;
+}
 
 #[no_mangle]
-fn rust_jump(val: u32) -> u32 {
-    val
+extern "C" fn rust_jump(_val: u32) -> u32 {
+    let a = 2;
+    let mastruc = Mastruct {lala:3, lolo:4};
+    unsafe {
+        c_jump(1, &a, mastruc);
+    }
+    return 42;
 }
