6e0e1f279b755a2f2bc4a8dcc158c54daac08d91
Mordak <bmickael@student.42.fr>
2019-01-14 19:23:19 +0000

    sandbox C to Rust

---
+++ sandbox/rust_to_c_and_asm/src/c/main.c
@@ -0,0 +1,5 @@
+extern int rust_jump(int i);
+
+int main(void) {
+	return rust_jump(42);
+}
---
+++ sandbox/rust_to_c_and_asm/src/lib.rs
@@ -0,0 +1,7 @@
+#![feature(lang_items)]
+#![feature(format_args_nl)]
+
+#[no_mangle]
+fn rust_jump(val: u32) -> u32 {
+    val
+}
