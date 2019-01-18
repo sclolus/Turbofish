9261f1f08d131538704da4826f0509064a398873
mordak <bmickael@student.42.fr>
2019-01-18 00:07:00 +0000

    Quantic behavior

--- rust_kernel/src/debug/debug_center.c
+++ rust_kernel/src/debug/debug_center.c
@@ -12,7 +12,7 @@ int debug_center(void) {
 	printk("Les carotes sont cuites\n");
 	printk("{red}Ce sont des choses qui arrivent{eoc}\n");
 	printk("Les carotes sont cuites\n");
-	printk("Ce sont des choses qui arrivent");
+	printk("Ce sont des choses qui arriventn");
 	while(1);
 	return get_cpu_features();
 }
