ca8c4915bb4e8e2f34b58dda614feef5f8d24d96
mordak <bmickael@student.42.fr>
2019-01-19 02:04:26 +0000

    Now allow to call 8086 bios 16bits interrupt

--- rust_kernel/src/rust_main.rs
+++ rust_kernel/src/rust_main.rs
@@ -1,13 +1,11 @@
 use crate::monitor::*;
 use crate::multiboot::{save_multiboot_info, MultibootInfo, MULTIBOOT_INFO};
-use crate::registers::*;
+use crate::registers::{real_mode_op, BaseRegisters};
 
 #[no_mangle]
 pub extern "C" fn kmain(multiboot_info: *const MultibootInfo) {
     clear_screen();
     save_multiboot_info(multiboot_info);
-
-    println!("ebp = {:?}", _get_ebp());
 
     println!("multiboot_infos {:#?}", MULTIBOOT_INFO);
     println!("base memory: {:?} {:?}", MULTIBOOT_INFO.unwrap().mem_lower, MULTIBOOT_INFO.unwrap().mem_upper);
@@ -25,18 +23,16 @@ pub extern "C" fn kmain(multiboot_info: *const MultibootInfo) {
     }
 
     let reg: BaseRegisters = BaseRegisters {
-        edi: 0x11111,
-        esi: 0x22222,
-        ebp: 0x33333,
-        esp: 0x44444,
-        ebx: 0x55555,
-        edx: 0x66666,
-        ecx: 0x77777,
-        eax: 0x88888,
+        edi: 0x1,
+        esi: 0x2,
+        ebp: 0x3,
+        esp: 0x4,
+        ebx: 0x118 | (1 << 14),
+        edx: 0x6,
+        ecx: 0x7,
+        eax: 0x4F02,
     };
     print!("value: ");
-    println!("esp = {:?}", _get_esp());
-    println!("ebp = {:?}", _get_ebp());
     println!("{:?}", real_mode_op(reg, 0x10));
     match set_cursor_position(4, 24) {
         Ok(()) => (),
