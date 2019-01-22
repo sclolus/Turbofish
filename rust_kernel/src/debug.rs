74f74cdcb770c47d65456ebedb2bf7e4c0bf3850
miniske <miniske@Debian-Seb-PC.Debian-Seb-PC>
2019-01-22 01:28:52 +0100

    Really added the src/debug.rs (Function name macro) this time

---
+++ rust_kernel/src/debug.rs
@@ -0,0 +1,13 @@
+pub const POISON_SLAB: u32 = 0x5a5a5a5a;
+
+macro_rules! function {
+    () => {{
+        fn f() {}
+        fn type_name_of<T>(_: T) -> &'static str {
+            extern crate core;
+            unsafe { core::intrinsics::type_name::<T>() }
+        }
+        let name = type_name_of(f);
+        &name[6..name.len() - 4]
+    }}
+}
