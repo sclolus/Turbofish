ef794c8e7e013c05e3b7a73495d5c90e258cb851
miniske <miniske@Debian-Seb-PC.Debian-Seb-PC>
2019-01-22 01:37:31 +0100

    Added some comments for the macro

--- rust_kernel/src/debug.rs
+++ rust_kernel/src/debug.rs
@@ -1,5 +1,11 @@
 pub const POISON_SLAB: u32 = 0x5a5a5a5a;
 
+// Returns a &[str] containing the full namespace specified name of the function
+
+// This works by declaring a dummy function f() nested in the current function.
+// Then by the type_name instrinsics, get the slice of the full specified name of the function f()
+// we then truncate the slice by the range notation to the name of the current function.
+// That is the slice with 5 characters removed.
 macro_rules! function {
     () => {{
         fn f() {}
