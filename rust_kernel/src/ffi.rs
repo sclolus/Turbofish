08e4c0c71c8205316bd44b5884e93628e70849bc
mordak <bmickael@student.42.fr>
2019-01-23 22:06:28 +0000

    Use typeAlias for c_char

--- rust_kernel/src/ffi.rs
+++ rust_kernel/src/ffi.rs
@@ -1,18 +1,10 @@
 use core::fmt;
 
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub struct c_char(pub u8);
-
-impl fmt::Debug for c_char {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.0 as char)
-    }
-}
+pub type c_char = u8;
 
 pub unsafe extern "C" fn strlen(ptr: *const c_char) -> usize {
     let mut i = 0;
-    while (*ptr.offset(i as isize)).0 != 0 {
+    while (*ptr.offset(i as isize)) != 0 {
         i += 1;
     }
     i
