9fa5b24d57199c05cdc1f27e6b77828626c4e9df
mordak <bmickael@student.42.fr>
2019-02-11 15:21:11 +0000

    Factorization of common_isr file with smart macros

--- rust_kernel/src/interrupts/idt/common_isr.asm
+++ rust_kernel/src/interrupts/idt/common_isr.asm
@@ -41,75 +41,59 @@ _get_pic_time:
 	lock cmpxchg [_pic_time], eax
 	ret
 
+; default ISR for all IDT entries
 global _default_isr
 _default_isr:
-    iret
+	iret
 
 ;; This generates the Interrupt service routines. The first paramater completes the indentifier
-;; The second paramater is the name of the interrupt as a string
-;; The third parameter is the rust function to call for handling the interrupt
-%macro CREATE_MASTER_ISR 3
+;; the first parameter identified if is a master pic or slave irq
+;; the second parameter compose the name of the exported symbol
+;; The third paramater is the name of the interrupt as a string
+;; The fourth parameter is the rust function to call for handling the interrupt
+%macro CREATE_ISR 4
 segment .data
-	isr_%1_str:
-	db %2, " interrupt", 0
+	isr_%2_str: db %3, " interrupt", 0
 segment .text
-global _isr_%1
-	_isr_%1:
+global _isr_%2
+_isr_%2:
 	push ebp
-	mov	ebp, esp
+	mov ebp, esp
 	pushad
-	push isr_%1_str
+	push isr_%2_str
 	push 4
-	push %3
+	push %4
 	call _align_stack
 	add esp, 12 ;pop interrupt string
-
-	mov al, 0x20
-	out 0x20, al
-
+	%1
 	popad
-	pop	ebp
+	pop ebp
 	iret
 %endmacro
 
-%macro CREATE_SLAVE_ISR 3
-segment .data
-	isr_%1_str:
-	db %2, " interrupt", 0
-segment .text
-global _isr_%1
-	_isr_%1:
-	push ebp
-	mov	ebp, esp
-	pushad
-	push isr_%1_str
-	push 4
-	push %3
-	call _align_stack
-	add esp, 12 ;pop interrupt string
-
+%macro MASTER 0
 	mov al, 0x20
 	out 0x20, al
+%endmacro
+
+%macro SLAVE 0
+	MASTER
 	mov al, 0xa0
 	out 0xa0, al
-
-	popad
-	pop	ebp
-	iret
 %endmacro
 
-	CREATE_MASTER_ISR keyboard, "Keyboard", keyboard_interrupt_handler
-	CREATE_MASTER_ISR cascade, "cascade, never used", generic_interrupt_handler ; should never be raised
-	CREATE_MASTER_ISR com2, "COM2", generic_interrupt_handler
-	CREATE_MASTER_ISR com1, "COM1", generic_interrupt_handler
-	CREATE_MASTER_ISR lpt2, "LPT2", generic_interrupt_handler
-	CREATE_MASTER_ISR floppy_disk, "floppy_disk", generic_interrupt_handler
-	CREATE_MASTER_ISR lpt1, "lpt1", generic_interrupt_handler 		; unreliable, often a spurious interrupt
+	; TIPS: use nasm -E file to view source file on stdout after macro expansion
+	CREATE_ISR MASTER, keyboard, "Keyboard", keyboard_interrupt_handler
+	CREATE_ISR MASTER, cascade, "cascade, never used", generic_interrupt_handler ; should never be raised
+	CREATE_ISR MASTER, com2, "COM2", generic_interrupt_handler
+	CREATE_ISR MASTER, com1, "COM1", generic_interrupt_handler
+	CREATE_ISR MASTER, lpt2, "LPT2", generic_interrupt_handler
+	CREATE_ISR MASTER, floppy_disk, "floppy_disk", generic_interrupt_handler
+	CREATE_ISR MASTER, lpt1, "lpt1", generic_interrupt_handler ; unreliable, often a spurious interrupt
 
-	CREATE_SLAVE_ISR cmos, "CMOS real-time clock", generic_interrupt_handler
-	CREATE_SLAVE_ISR acpi, "ACPI", generic_interrupt_handler
-	CREATE_SLAVE_ISR ps2_mouse, "PS/2 mouse", generic_interrupt_handler
-	CREATE_SLAVE_ISR fpu_coproc, "FPU / Coproc / inter-processor", generic_interrupt_handler
-	CREATE_SLAVE_ISR primary_hard_disk, "Primary ATA hard disk", generic_interrupt_handler
-	CREATE_SLAVE_ISR secondary_hard_disk, "Secondary ATA hard disk", generic_interrupt_handler
-
+	CREATE_ISR SLAVE, cmos, "CMOS real-time clock", generic_interrupt_handler
+	CREATE_ISR SLAVE, acpi, "ACPI", generic_interrupt_handler
+	CREATE_ISR SLAVE, ps2_mouse, "PS/2 mouse", generic_interrupt_handler
+	CREATE_ISR SLAVE, fpu_coproc, "FPU / Coproc / inter-processor", generic_interrupt_handler
+	CREATE_ISR SLAVE, primary_hard_disk, "Primary ATA hard disk", generic_interrupt_handler
+	CREATE_ISR SLAVE, secondary_hard_disk, "Secondary ATA hard disk", generic_interrupt_handler
--- rust_kernel/src/interrupts/idt/idt.asm
+++ rust_kernel/src/interrupts/idt/idt.asm
@@ -7,15 +7,14 @@ global _get_idtr
 
 ;; Loads a specific `struct Idtr` in the Interrupt Descriptor Table Register
 _load_idtr:
-
 ;; The only parameter is the address of `struct Idtr`
 ;; passed by the rust _load_idtr routine
-	mov	eax, [dword esp + 4]
-	lidt	[eax] ;; Load Idtr struct in the idt register
+	mov eax, [dword esp + 4]
+	lidt [eax] ;; Load Idtr struct in the idt register
 	ret
 
 ;; Fills the `struct Idtr` which is passed as an address
 _get_idtr:
-	mov	eax, [dword esp + 4]
-	sidt	[eax]
+	mov eax, [dword esp + 4]
+	sidt [eax]
 	ret
