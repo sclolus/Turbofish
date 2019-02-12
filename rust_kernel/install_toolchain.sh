e926dd08fcbfa415a6fd0e954341c30c552a285d
vcombey <vcombey@student.42.fr>
2019-02-12 19:57:49 +0100

    rust-toolchain file instead of rustup overide, install_toolchain.sh

--- rust_kernel/install_toolchain.sh
+++ rust_kernel/install_toolchain.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-RUSTUP_VERSION="nightly-2019-02-10"
 CARGO_XBUILD_VERSION="0.5.5"
 
-rustup override set $RUSTUP_VERSION
+curl https://sh.rustup.rs -sSf | sh -s -- --default-host i686-unknown-linux-gnu --default-toolchain nightly
+source $HOME/.cargo/env  
+rustup toolchain add nightly
+rustup target add i686-unknown-linux-gnu
 rustup component add rust-src
+
 res=`cargo install --list | grep "cargo-xbuild v$CARGO_XBUILD_VERSION"`
 if [ -z "$res" ]
 then
