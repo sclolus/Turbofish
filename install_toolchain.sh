600d6aad6f5e689055847e5f101f62d447c105b3
vcombey <vcombey@student.42.fr>
2019-02-14 16:49:36 +0100

    add gcc, nasm, make.. in instal_toolchain.sh

--- install_toolchain.sh
+++ install_toolchain.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
-CARGO_XBUILD_VERSION="0.5.5"
 
 curl https://sh.rustup.rs -sSf | sh -s -- --default-host i686-unknown-linux-gnu --default-toolchain nightly
-source $HOME/.cargo/env  
+source $HOME/.cargo/env
 rustup toolchain add nightly
 rustup target add i686-unknown-linux-gnu
 rustup component add rust-src
-
-res=`cargo install --list | grep "cargo-xbuild v$CARGO_XBUILD_VERSION"`
-if [ -z "$res" ]
-then
-	cargo install --version $CARGO_XBUILD_VERSION cargo-xbuild --force
-else
-	echo "cargo-xbuild $CARGO_XBUILD_VERSION already installed"
-fi
-exit 0
-
-sudo apt-get install qemu-system-x86
+rustup component add rust-fmt
+cargo install cargo-xbuild
+sudo apt-get install qemu-system-x86 make gcc nasm
