717924824388d9b1d07f21af4e2c8983df925e67
mordak <bmickael@student.42.fr>
2019-02-15 22:02:43 +0000

    math common error result

--- rust_kernel/src/math.rs
+++ rust_kernel/src/math.rs
@@ -1,3 +1,14 @@
 /// number conversion like rand or trunc
 pub mod convert;
 pub mod rand;
+
+#[derive(Debug, Copy, Clone)]
+pub enum MathError {
+    OutOfBound,
+    DivideByZero,
+    Overflow,
+    Infinity,
+    Unsupported,
+}
+
+pub type MathResult<T> = core::result::Result<T, MathError>;
