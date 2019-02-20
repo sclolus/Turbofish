0145763e2f45e2865f198dc24498ce5eca84f7b8
miniske <miniske@Debian-Seb-PC.Debian-Seb-PC>
2019-02-20 01:44:26 +0100

    Fixed panic in case of invalid command line, changed the usage print to eprint

--- rust_kernel/integration_tester/src/main.rs
+++ rust_kernel/integration_tester/src/main.rs
@@ -12,7 +12,7 @@ const TIMEOUT: Duration = Duration::from_secs(20);
 
 fn print_usage(program: &str, opts: Options) {
     let brief = format!("Usage: {} FILE [options]", program);
-    print!("{}", opts.usage(&brief));
+    eprint!("{}", opts.usage(&brief));
 }
 
 enum TestError {
@@ -28,7 +28,10 @@ fn main() {
     opts.optflag("h", "help", "print this help menu");
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => m,
-        Err(f) => panic!(f.to_string()),
+        Err(f) => {
+            print_usage(&program, opts);
+            std::process::exit(1);
+        }
     };
     if matches.opt_present("h") {
         print_usage(&program, opts);
