8327be69df82b1103859f60ccf71d0c1dac063b9
mordak <bmickael@student.42.fr>
2019-02-20 22:25:52 +0000

    round little test

--- rust_kernel/src/math/convert.rs
+++ rust_kernel/src/math/convert.rs
@@ -42,3 +42,27 @@ impl Convert for f64 {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::math::convert::Convert;
+    #[test]
+    fn test_round() {
+        let array: [(f64, f64); 10] = [
+            (3.14, 3.),
+            (-895.11, -895.),
+            (0.49, 0.),
+            (core::f64::INFINITY, core::f64::INFINITY),
+            (0., 0.),
+            (4_294_967_295.508, 4_294_967_296.0),
+            (4_294_967_295.487, 4_294_967_295.0),
+            (-999.51, -1000.),
+            (-999.49, -999.),
+            (-999.0, -999.0),
+        ];
+
+        for i in array.iter() {
+            assert_eq!(f64::round(i.0), i.1);
+        }
+    }
+}
