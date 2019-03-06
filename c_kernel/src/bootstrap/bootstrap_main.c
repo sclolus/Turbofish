69a136aacc5d638c38d8edbce8c72e028fd36ed3
mordak <bmickael@student.42.fr>
2019-03-06 07:22:11 +0000

    printk available on Low memory

--- c_kernel/src/bootstrap/bootstrap_main.c
+++ c_kernel/src/bootstrap/bootstrap_main.c
@@ -1,9 +1,17 @@
 
 #include "libft.h"
 
+#include "vga_text.h"
+
 extern void bootstrap_end(void);
+
+u8 banane[1024] = {0};
 
 void bootstrap_main(void) {
 	ft_memset(0, 0, 0);
+	banane[0] = 42;
+	clear_screen();
+	printk("Les carotes sont cuites\n");
+	while (1) {}
 	bootstrap_end();
 }
