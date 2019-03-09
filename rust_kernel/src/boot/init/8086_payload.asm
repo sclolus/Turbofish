f155758dd7b541b1d2ce1d9dbec787ff9265ac7d
mordak <bmickael@student.42.fr>
2019-03-09 07:18:17 +0000

    Give the device map structure to Rust Kernel
    - Add a kernel part in Low Memory

---
+++ rust_kernel/src/boot/init/8086_payload.asm
@@ -0,0 +1,314 @@
+
+	; This function allow to execute a 16bits real mode payload from 32 bits protection mode
+
+	; extern int i8086_payload(struct BaseRegisters regs, void *payload, size_t payload_len);
+	; return of -1 if payload is to big
+
+	; pub struct BaseRegisters {
+	;     /*0        |*/ pub edi:u32,
+	;     /*4        |*/ pub esi:u32,
+	;     /*8        |*/ pub ebp:u32,
+	;     /*12       |*/ pub esp:u32,
+	;     /*16       |*/ pub ebx:u32,
+	;     /*20       |*/ pub edx:u32,
+	;     /*24       |*/ pub ecx:u32,
+	;     /*28       |*/ pub eax:u32,
+	;     /*32       |*/
+	; }
+
+	; Rust calling convention i386: The first parameter is close than EIP, obviously, access is EBP + 8
+	; for example, fn (x:u32. y:u32) -> u32, after pushing EBP, x is on (EBP + 8) and y is on (EBP + 12)
+	; the classical return is the EAX register. Caution: Never return structure between two compiler
+	; ---------
+	; | ARG 2 |
+	; +-------+
+	; | ARG 1 |
+	; +-------+
+	; |  EIP  | (EIP of caller)
+	; +-------+
+	; |  EBP  | (After pushing EBP by first instruction of ASM function)
+	; +-------+
+
+	; 32 bits protected to real 16 bits steps by steps
+	; ----------------------------------------------------------------------------------------------------------------------------------------
+	; Disable the interrupts:
+	;     Turn off maskable interrupts using CLI.
+	;     Disable NMI (optional).
+	; Turn off paging: IMPORTANT: In this below impementation Paging is shut down at the same moment of protected mode. CR3 fflush is juste before it.
+	;     Transfer control to a 1:1 page.
+	;     Ensure that the GDT and IDT are in a 1:1 page.
+	;     Clear the PG-flag in the zeroth control register.
+	;     Set the third control register to 0.
+	; Use GDT with 16-bit tables (skip this step if one is already available):
+	; Create a new GDT with a 16-bit data and code segment:
+	;     Limit: 0xFFFFF
+	;     Base: 0x0
+	;     16-bit
+	;     Privilege level: 0
+	;     Granularity: 0
+	;     Read and Write: 1
+	; Load new GDT ensuring that the currently used selectors will remain the same (index in cs/ds/ss will be copy of original segment in new GDT)
+	; Far jump to 16-bit protected mode:
+	;     Far jump to 16-bit protected mode with a 16-bit segment index.
+	;     Load data segment selectors with 16-bit indexes:
+	;     Load ds, es, fs, gs, ss with a 16-bit data segment.
+	; Load real mode IDT:
+	;     Limit: 0x3FF
+	;     Base 0x0
+	;     Use lidt
+	; Disable protected mode: IMPORTANT: In this below implementation, paging is deactivate with Protected mode, in the same moment.
+	;     Set PE bit in CR0 to false.
+	;     Far jump to real mode:
+	;     Far jump to real mode with real mode segment selector (usually 0).
+	;     Reload data segment registers with real mode values:
+	;     Load ds, es, fs, gs, ss with appropriate real mode values (usually 0).
+	; Set stack pointer to appropriate value:
+	;     Set sp to stack value that will not interfere with real mode program.
+	; Enable interrupts:
+	;     Enable maskable interrupts with STI.
+	;     Continue on in real mode with all bios interrupts.
+
+	; CAUTION
+	; If enabled, The PIC must be disabled before calling this code
+	; The code for paging is disabled, uncomment it if you have to manage with paging
+
+[BITS 32]
+segment .text
+
+%define ALL_REGISTERS_OFFSET 32 ; popad and pushas modufication offset for esp
+
+; POPAD and PUSHAD operations conerned ALL registers except ESP, which is normal behav +32, -32
+
+%define BASE_LOCATION       0x7C00    ; Payload will be copied at that address
+%define PAYLOAD_MAX_LEN     8192
+%define REBASE(x)           (BASE_LOCATION + x - begin_sub_sequence)
+
+; int i8086_payload(Base_registers reg, void *payload, size_t payload_len);
+GLOBAL i8086_payload
+i8086_payload:
+	push ebp
+	mov ebp, esp
+
+	; preserve all caller registers
+	pushad
+
+	; copy of content at BASE_LOCATION
+	mov eax, end_sub_sequence
+	sub eax, begin_sub_sequence
+	mov ecx, eax
+	mov esi, begin_sub_sequence
+	mov edi, BASE_LOCATION
+	cld
+	rep movsb
+
+	; copy the payload
+	; args [ebp + 8 + 32] -> payload * et [ebp + 12 + 32] -> len
+	; 32 = ALL_REGISTERS_OFFSET
+	mov eax, [ebp + 8 + ALL_REGISTERS_OFFSET]
+	mov esi, eax
+	mov eax, REBASE(begin_sub_sequence.payload)
+	mov edi, eax
+	mov ecx, [ebp + 12 + ALL_REGISTERS_OFFSET]
+	; test if payload is to big
+	mov eax, -1
+	cmp ecx, PAYLOAD_MAX_LEN
+	ja .end
+	rep movsb
+
+	; initialise temporary GDT
+	mov eax, gdt_16_end
+	sub eax, gdt_16
+	mov word [REBASE(gdt_16_ptr)], ax
+
+	; store linear address of GDT
+	mov eax, gdt_16
+	mov dword [REBASE(gdt_16_ptr + 2)], eax
+
+	; put ESP on the first argument
+	add esp, 8 + ALL_REGISTERS_OFFSET
+
+	; Get all arguments registers
+	popad
+
+	; realign ESP on current level, popad has incremented esp by 32
+	sub esp, 8 + ALL_REGISTERS_OFFSET + ALL_REGISTERS_OFFSET
+
+	; recovery of current EBP : (esp is preserved by popad operation)
+	push eax
+	mov eax, [esp + 12]
+	mov ebp, eax
+	pop eax
+
+	; push a address to join after execution with instruction ret
+	call BASE_LOCATION
+
+.end:
+	; store return EAX
+	mov [esp + 28], eax
+
+	; restore all registers values
+	popad
+
+	pop ebp
+ret
+
+	; -------------------------------------------------
+	; *** This part is copied in BASE_LOCATION area ***
+	; -------------------------------------------------
+begin_sub_sequence:
+	; saving of all data segments register
+	mov [REBASE(_ds)], ds
+	mov [REBASE(_es)], es
+	mov [REBASE(_fs)], fs
+	mov [REBASE(_gs)], gs
+	mov [REBASE(_ss)], ss
+
+	; saving of current CS segment
+	mov [REBASE(.cs_value_location)], cs
+
+	; store AX parameter
+	mov [REBASE(_eax)], eax
+
+	; store ESP and EBP because 16bit payload could change SP and BP value
+	mov [REBASE(_esp)], esp
+	mov [REBASE(_ebp)], ebp
+
+	; store CR3 parameter
+	; mov eax, cr3
+	; mov [REBASE(_cr3)], eax
+
+	; store caller idt and load BIOS idt
+	sidt [REBASE(saved_idtptr)]
+	lidt [REBASE(bios_idt)]
+
+	; store caller gdt and load custom 16 bits gdt
+	sgdt [REBASE(saved_gdtptr)]
+	lgdt [REBASE(gdt_16_ptr)]
+
+	; jump to CS of 16 bits selector
+	jmp 0x8:REBASE(.protected_16)
+.protected_16:
+	; code is now in 16bits, because we are in 16 bits mode
+[BITS 16]
+
+	; set 16 bits protected mode data selector
+	mov  ax, 0x10
+	mov  ds, ax
+	mov  es, ax
+	mov  fs, ax
+	mov  gs, ax
+	mov  ss, ax
+
+	; disable protected bit
+	mov eax, cr0
+	and eax, 0xfffffffe
+	mov cr0, eax
+
+	; disable paging (PG) && protected bit
+	; mov eax, cr0
+	; and eax, 0x7ffffffe
+	; mov cr0, eax
+
+	; fflush CR3 register
+	; xor eax, eax
+	; mov cr3, eax
+
+	; configure CS in real mode
+	jmp 0x0:REBASE(.real_16)
+.real_16:
+	; configure DS, ES and SS in real mode
+	xor ax, ax
+	mov ds, ax
+	mov es, ax
+	mov ss, ax
+
+	; take saved eax
+	mov eax, [REBASE(_eax)]
+
+	; enable interupts
+	sti
+
+.payload:
+	times PAYLOAD_MAX_LEN nop
+	; disable interupt
+	cli
+
+	; load caller idt and caller gdt
+	lidt [REBASE(saved_idtptr)]
+	lgdt [REBASE(saved_gdtptr)]
+
+	; entering in protected mode
+	mov ebx, cr0
+	or  bx, 1
+	mov cr0, ebx     ; PE set to 1 (CR0)
+
+	; configure CS in protected mode
+	; Eq: jmp 0x8:REBASE(.protected_32) with CS value as 0x8
+	; 0xEA is the opcode of long jump -> jmp ptr16:u16
+	db 0xEA
+	dw REBASE(.protected_32)
+.cs_value_location:
+	dw 0xBEEF
+.protected_32:
+
+	; code is now in 16bits
+[BITS 32]
+	; restore all segments registers
+	mov ds, [REBASE(_ds)]
+	mov es, [REBASE(_es)]
+	mov fs, [REBASE(_fs)]
+	mov gs, [REBASE(_gs)]
+	mov ss, [REBASE(_ss)]
+
+	; recover saved esp and ebp
+	mov esp, [REBASE(_esp)]
+	mov ebp, [REBASE(_ebp)]
+
+	; restore Paging
+	; mov ebx, [REBASE(_cr3)]     ; restore CR3 Page directory Phy address location
+	; mov cr3, ebx
+
+	; mov ebx, cr0
+	; or ebx, 0x80000001          ; restore PG bit (Protected bit must be enable with it)
+	; mov cr0, ebx
+
+	; return to base function
+	ret
+
+bios_idt:
+	dw 0x3ff ; limit
+	dd 0     ; base
+saved_idtptr:
+	dw 0
+	dd 0
+saved_gdtptr:
+	dw 0     ; limit
+	dd 0     ; base
+
+; _cr3: dd 0xFEEDBABE
+
+_esp: dd 0xDEADBEEF
+_ebp: dd 0xDEADBEEF
+
+_eax: dd 0xDEADBEEF
+_ds: dw 0xBEEF
+_es: dw 0xBEEF
+_fs: dw 0xBEEF
+_gs: dw 0xBEEF
+_ss: dw 0xBEEF
+
+gdt_16:
+	db 0, 0, 0, 0, 0, 0, 0, 0
+.gdt_16b_cs:
+	dw 0xFFFF, 0x0000
+	db 0x00, 0x9A, 0x0, 0x0
+.gdt_16b_ds:
+	dw 0xFFFF, 0x0000
+	db 0x00, 0x92, 0x0, 0x0
+gdt_16_end:
+
+gdt_16_ptr:
+	dw 0xBEEF      ; limit
+	dd 0xFEEDBABE  ; base
+
+end_sub_sequence:
---
+++ rust_kernel/src/boot/init/init_paging.asm
@@ -0,0 +1,109 @@
+[BITS 32]
+
+%include "src/boot/init/early_paging.asm"
+
+segment .text
+
+%define KERNEL_MAX_SIZE 64
+
+; from https://web.archive.org/web/20130609073242/http://www.osdever.net/tutorials/rm_addressing.php?the_id=50
+
+; The x86-16/32 CPU Runs in Two Modes - RealMode (16-Bit) and ProtectedMode (32-Bit),
+; (There are a few big Differences Between these modes - The HUGE Difference
+; is the way Memory is handled - We will not explain the other differences:
+;     This information is beyond the purpose of this tutorial, We will also not explain
+;     anything about ProtectedMode in this tutorial - As this tutorial is all about RealMode.)
+; NOTE: On the x86 Platform, the CPU (Central Processing Unit) Boots into RealMode.
+
+; On the x86 Platform there is something called Address Space
+; This is space that maps out Memory on various devices
+; It mostly maps out RAM (Randon Acess Memory - The Computer's Main Memory)
+; But it also maps other Memory such as: Video Memory (Video RAM),
+; The BIOS ROM's (Read Only Memory) - There are some other things aswell.
+; In RealMode you can only acess 1mb of Address Space
+; This 1mb Maps out RAM, Video RAM, and the BIOS ROM's - Some of the memory is also taken up.
+
+; Below is a Memory-Map of RealMode Memory, it shows what is located at each Memory Address (In RealMode)
+; It also shows if that Memory Address is free for system use
+; (When i say "System Use" i am referring to the State of the Memory when the System "Boots"
+; You might not Understand what this Map means right now - But it will help later on, So you might want to come back to it later);
+
+; Mapping of the first mo
+
+; 0x0000:0x0000 -> 0x0000:0x03FF = IVT (Interrupt Vector Table)
+; 0x0000:0x0400 -> 0x0000:0x04FF = BDA (BIOS Data Area)
+
+; 0x0000:0x0500 -> 0x0000:0x7BFF = Free Useable Memory!
+
+; 0x0000:0x7C00 -> 0x0000:0x7DFF = Operating System BootSector - This is where the BIOS
+; Loads your Operating System's BootSector at Boot Time (You can use this Memory, as long as
+; your BootSector isn't executing and you don't need your BootSector anymore!)
+
+; 0x0000:0x7E00 -> 0x9000:0xFFFF = Free Useable Memory!
+
+; 0xA000:0x0000 -> 0xB000:0xFFFF = VGA Video RAM
+; 0xC000:0x0000 -> 0xF000:0xFFFF = BIOS ROM Memory Area
+
+; 0xFFFF:0x0010 -> 0xFFFF:0xFFFF = Free Useable Memory (If the A20 Gate is Enabled) - This
+; Memory is Above the 1mb Mark, 0xFFFF:0x0010 = 0x00100000(1mb)
+
+; The grub bootloader let the kernel in 32 bits protected mode here:
+
+global init_paging
+init_paging:
+	push ebp
+	mov ebp, esp
+	; block interrupts
+
+	; Paginate kernel in half high memory (do also identity mapping)
+	INIT_PAGING_ENV
+
+	; 0x00000000 -> 0x04000000 mapped to phy 0x00000000 -> 0x04000000
+
+%define l0_virt_offset 0
+%define l0_physic_addr 0
+%define l0_len KERNEL_MAX_SIZE
+
+	PAGINATE_ADDR l0_virt_offset, l0_physic_addr, l0_len
+
+	; 0xC0000000 -> 0xC4000000 mapped to phy 0x00000000 -> 0x04000000
+
+%define l1_virt_offset 768
+%define l1_physic_addr 0
+%define l1_len KERNEL_MAX_SIZE
+
+	PAGINATE_ADDR l1_virt_offset, l1_physic_addr, l1_len
+
+	; Active paging
+	mov eax, page_directory_alpha_area
+	mov cr3, eax ; fill CR3 with physic mem pointer to page directory
+
+	mov eax, cr0
+	or eax, 0x80000001          ; enable Paging bit (PG). Protection bit must be also recall here
+	mov cr0, eax
+
+	pop ebp
+	ret
+
+%define VIRTUAL_LINEAR_FB_LOCATION 0xF0000000
+
+; 0xF0000000 -> ... mapped to phy ??? -> ??? + LFB_SIZE
+; hack for LFB allocation
+; CAUTION: Usable only when high memory is initialized
+global _allocate_linear_frame_buffer
+_allocate_linear_frame_buffer:
+	push ebp
+	mov ebp, esp
+
+	push dword [ebp + 12]               ; len
+	push dword [ebp + 8]                ; physical address
+	push (1024 - 64)                    ; virt addr offset. eq 0xF0000000
+
+	call _dynamic_map
+
+	add esp, 12
+
+	mov eax, VIRTUAL_LINEAR_FB_LOCATION
+
+	pop ebp
+	ret
