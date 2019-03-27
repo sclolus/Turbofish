3653b13e358af5055bee397647ae85961a8f96c5
vcombey <vcombey@student.42.fr>
2019-03-27 13:13:21 +0100

    mutexione the Pit

--- rust_kernel/src/test_helpers/really_lazy_hello_world.rs
+++ rust_kernel/src/test_helpers/really_lazy_hello_world.rs
@@ -3,73 +3,26 @@ use crate::monitor::{Color, SCREEN_MONAD};
 use core::time::Duration;
 
 pub fn really_lazy_hello_world() {
-    unsafe {
-        PIT0.sleep(Duration::from_millis(200));
-        SCREEN_MONAD.lock().set_text_color(Color::Green).unwrap();
+    use Color::*;
+    let hello_world = [
+        ("H", Green),
+        ("E", Red),
+        ("L", Blue),
+        ("L", Yellow),
+        ("O", Cyan),
+        (" ", Brown),
+        ("W", Magenta),
+        ("O", Green),
+        ("R", Red),
+        ("L", Blue),
+        ("D", Yellow),
+        (" ", Cyan),
+        ("!", White),
+    ];
+    for (c, color) in hello_world.iter() {
+        PIT0.lock().sleep(Duration::from_millis(200));
+        SCREEN_MONAD.lock().set_text_color(*color).unwrap();
+        print!("{}", c);
     }
-    print!("H");
-    unsafe {
-        PIT0.sleep(Duration::from_millis(200));
-        SCREEN_MONAD.lock().set_text_color(Color::Red).unwrap();
-    }
-    print!("E");
-    unsafe {
-        PIT0.sleep(Duration::from_millis(200));
-        SCREEN_MONAD.lock().set_text_color(Color::Blue).unwrap();
-    }
-    print!("L");
-    unsafe {
-        PIT0.sleep(Duration::from_millis(200));
-        SCREEN_MONAD.lock().set_text_color(Color::Yellow).unwrap();
-    }
-    print!("L");
-    unsafe {
-        PIT0.sleep(Duration::from_millis(200));
-        SCREEN_MONAD.lock().set_text_color(Color::Cyan).unwrap();
-    }
-    print!("O");
-    unsafe {
-        PIT0.sleep(Duration::from_millis(200));
-        SCREEN_MONAD.lock().set_text_color(Color::Brown).unwrap();
-    }
-    print!(" ");
-    unsafe {
-        PIT0.sleep(Duration::from_millis(200));
-        SCREEN_MONAD.lock().set_text_color(Color::Magenta).unwrap();
-    }
-    print!("W");
-    unsafe {
-        PIT0.sleep(Duration::from_millis(200));
-        SCREEN_MONAD.lock().set_text_color(Color::White).unwrap();
-    }
-    print!("O");
-    unsafe {
-        PIT0.sleep(Duration::from_millis(200));
-        SCREEN_MONAD.lock().set_text_color(Color::Green).unwrap();
-    }
-    print!("R");
-    unsafe {
-        PIT0.sleep(Duration::from_millis(200));
-        SCREEN_MONAD.lock().set_text_color(Color::Red).unwrap();
-    }
-    print!("L");
-    unsafe {
-        PIT0.sleep(Duration::from_millis(200));
-        SCREEN_MONAD.lock().set_text_color(Color::Blue).unwrap();
-    }
-    print!("D");
-    unsafe {
-        PIT0.sleep(Duration::from_millis(200));
-        SCREEN_MONAD.lock().set_text_color(Color::Yellow).unwrap();
-    }
-    print!(" ");
-    unsafe {
-        PIT0.sleep(Duration::from_millis(200));
-        SCREEN_MONAD.lock().set_text_color(Color::Cyan).unwrap();
-    }
-    println!("!");
-    unsafe {
-        PIT0.sleep(Duration::from_millis(200));
-        SCREEN_MONAD.lock().set_text_color(Color::White).unwrap();
-    }
+    print!("\n");
 }
