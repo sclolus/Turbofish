51289ba5364b61cd5b980b3667ccb5407db7f43c
miniske <miniske@Debian-Seb-PC.Debian-Seb-PC>
2019-04-04 22:22:42 +0200

    Moved the bootstrap allocator in the new kernel_allocator module

--- rust_kernel/src/memory/allocator/kernel_allocator.rs
+++ rust_kernel/src/memory/allocator/kernel_allocator.rs
@@ -9,37 +9,14 @@ use alloc::boxed::Box;
 use alloc::vec;
 use core::alloc::{GlobalAlloc, Layout};
 
+mod bootstrap;
+use bootstrap::*;
+
 pub static mut KERNEL_ALLOCATOR: KernelAllocator = KernelAllocator::Bootstrap(BootstrapKernelAllocator::new());
 
 pub enum KernelAllocator {
     Bootstrap(BootstrapKernelAllocator),
     Kernel(SlabAllocator),
-}
-
-/// 4 MB for the bootstrap
-const MEMORY_BOOTSTRAP_KERNEL_ALLOCATOR: usize = 0x400_000;
-
-static mut BSS_MEMORY: [u8; MEMORY_BOOTSTRAP_KERNEL_ALLOCATOR] = [0; MEMORY_BOOTSTRAP_KERNEL_ALLOCATOR];
-
-#[derive(Debug)]
-pub struct BootstrapKernelAllocator {
-    current_offset: usize,
-}
-
-impl BootstrapKernelAllocator {
-    pub const fn new() -> Self {
-        BootstrapKernelAllocator { current_offset: 0 }
-    }
-    pub unsafe fn alloc_bootstrap(&mut self, layout: Layout) -> Result<Virt> {
-        let base_address = &BSS_MEMORY[0] as *const u8 as usize;
-        let mut address = Virt(&BSS_MEMORY[self.current_offset] as *const u8 as usize);
-        address = address.align_next(layout.align());
-        self.current_offset = address.0 - base_address + layout.size();
-        if self.current_offset > BSS_MEMORY.len() {
-            panic!("No more bootstrap memory");
-        }
-        Ok(address)
-    }
 }
 
 pub struct RustGlobalAlloc;
