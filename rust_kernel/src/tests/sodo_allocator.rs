dcef2907c0f322e9561869531e6fdb9f3c6c7b93
miniske <miniske@Debian-Seb-PC.Debian-Seb-PC>
2019-04-04 10:38:35 +0200

    Supposedly fixed the integration tests too

--- rust_kernel/src/tests/sodo_allocator.rs
+++ rust_kernel/src/tests/sodo_allocator.rs
@@ -1,7 +1,7 @@
 use crate::interrupts;
 use crate::io::UART_16550;
 use crate::memory;
-use crate::memory::allocator::physical_page_allocator::DeviceMap;
+use crate::memory::tools::DeviceMap;
 use crate::multiboot::MultibootInfo;
 use crate::tests::helpers::exit_qemu;
 
@@ -16,7 +16,8 @@ pub extern "C" fn kmain(multiboot_info: *const MultibootInfo, device_map_ptr: *c
     }
     crate::watch_dog();
     unsafe {
-        memory::init_memory_system(multiboot_info.get_memory_amount_nb_pages(), device_map_ptr).unwrap();
+        let device_map = crate::memory::tools::get_device_map_slice(device_map_ptr);
+        memory::init_memory_system(multiboot_info.get_memory_amount_nb_pages(), device_map).unwrap();
     }
     crate::watch_dog();
 
