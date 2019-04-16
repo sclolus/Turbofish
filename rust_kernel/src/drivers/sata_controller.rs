1c2baf98109adcb0a7acec6ffdc0e091249cf03b
mordak <bmickael@student.42.fr>
2019-04-16 08:08:58 -0400

    Add some boilerplates

--- rust_kernel/src/drivers/sata_controller.rs
+++ rust_kernel/src/drivers/sata_controller.rs
@@ -5,6 +5,67 @@ use super::{
 
 use super::PciType0;
 
+#[derive(Debug, Copy, Clone)]
+#[repr(packed)]
+struct HbaMem {
+    // 0x00 - 0x2B, Generic Host Control
+    /*0        |*/ cap: u32, // Host capability
+    /*4        |*/ ghc: u32, // Global host control
+    /*8        |*/ is: u32, // Interrupt status
+    /*c        |*/ pi: u32, // Port implemented
+    /*10       |*/ vs: u32, // Version
+    /*14       |*/ ccc_ctl: u32, // Command completion coalescing control
+    /*18       |*/ ccc_pts: u32, // Command completion coalescing ports
+    /*1c       |*/ en_loc: u32, // Enclosure management location
+    /*20       |*/ en_ctl: u32, // Enclosure management control
+    /*24       |*/ cap2: u32, // Host capabilities extended
+    /*28       |*/ bohc: u32, // BIOS/OS handoff control and status
+
+    // 0x2C - 0x9F, Reserved
+    /*2C       |*/ reserved: Reserved,
+
+    // 0xA0 - 0xFF, Vendor specific registers
+    /*A0       |*/
+    vendor_specific_registers: VendorSpecificRegisters,
+
+    // 0x100 - 0x10FF, Port control registers
+    /*100      |*/ ports: [HbaPort; 32],
+}
+
+define_raw_data!(Reserved, 0xA0 - 0x2C);
+define_raw_data!(VendorSpecificRegisters, 0x100 - 0xA0);
+
+#[derive(Debug, Copy, Clone)]
+#[repr(packed)]
+struct HbaPort {
+    /*0        |*/ clb: u32, // command list base address, 1K-byte aligned
+    /*4        |*/ clbu: u32, // command list base address upper 32 bits
+    /*8        |*/ fb: u32, // FIS base address, 256-byte aligned
+    /*c        |*/ fbu: u32, // FIS base address upper 32 bits
+    /*10       |*/ is: u32, // interrupt status
+    /*14       |*/ ie: u32, // interrupt enable
+    /*18       |*/ cmd: u32, // command and status
+    /*1c       |*/ rsv0: u32, // Reserved
+    /*20       |*/ tfd: u32, // task file data
+    /*24       |*/ sig: u32, // signature
+    /*28       |*/ ssts: u32, // SATA status (SCR0:SStatus)
+    /*2c       |*/ sctl: u32, // SATA control (SCR2:SControl)
+    /*30       |*/ serr: u32, // SATA error (SCR1:SError)
+    /*34       |*/ sact: u32, // SATA active (SCR3:SActive)
+    /*38       |*/ ci: u32, // command issue
+    /*3c       |*/ sntf: u32, // SATA notification (SCR4:SNotification)
+    /*40       |*/ fbs: u32, // FIS-based switch control
+
+    // 0x44 ~ 0x6F, Reserved
+    /*10       |*/ reserved: ReservedPort,
+
+    // 0x70 ~ 0x7F, vendor specific
+    /*10       |*/ vendor_specific: VendorSpecificPort,
+}
+
+define_raw_data!(ReservedPort, 0x70 - 0x44);
+define_raw_data!(VendorSpecificPort, 0x80 - 0x70);
+
 #[derive(Copy, Clone, Debug)]
 pub struct SataController {
     pci: PciType0,
