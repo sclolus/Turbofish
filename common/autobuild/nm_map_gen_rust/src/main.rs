c627352aa531af13cffa2973f83c3e137f09bb84
vcombey <vcombey@student.42.fr>
2019-04-18 20:59:03 +0200

    handle 1 and 2 args

--- common/autobuild/nm_map_gen_rust/src/main.rs
+++ common/autobuild/nm_map_gen_rust/src/main.rs
@@ -9,40 +9,28 @@ struct Symbol<'a> {
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    if args.len() != 3 && args.len() != 2 {
-        panic!("bad argument number, args: {:?}", args);
-    }
+    assert!(args.len() == 2 || args.len() == 3, "bad number of args");
     let mut res_file = std::fs::File::create(&args[1]).unwrap();
-    let nm_file = std::fs::read_to_string(&args[2]).unwrap();
-    let symbols: Vec<Symbol> = nm_file
-        .lines()
-        .map(|x| {
-            let mut s = x.split_whitespace();
-            Symbol {
-                addr: s.next().unwrap(),
-                t: s.next().unwrap(),
-                name: s.next().unwrap_or(""),
-            }
-        })
-        .filter(|s| s.t != "U")
-        .collect();
-    let nb_symbol = symbols.len();
-    res_file
-        .write_fmt(format_args!("#define FN_DIR_LEN	{}\n", nb_symbol))
-        .unwrap();
-    res_file
-        .write_fmt(format_args!(
-            "static struct symbol_entry function_directory[{}] = {{",
-            nb_symbol,
-        ))
-        .unwrap();
-    for s in symbols {
-        res_file
-            .write_fmt(format_args!(
-                "\t{{0x{}, '{}', \"{}\"}},\n",
-                s.addr, s.t, s.name
-            ))
-            .unwrap();
+    if args.len() == 2 {
+        let nb_symbol = 0;
+        write!(res_file, "#define FN_DIR_LEN	{}\n", nb_symbol).unwrap();
+        write!(res_file, "static struct symbol_entry function_directory[{}] = {{}};\n", nb_symbol,).unwrap();
+    } else if args.len() == 3 {
+        let nm_file = std::fs::read_to_string(&args[2]).unwrap();
+        let symbols: Vec<Symbol> = nm_file
+            .lines()
+            .map(|x| {
+                let mut s = x.split_whitespace();
+                Symbol { addr: s.next().unwrap(), t: s.next().unwrap(), name: s.next().unwrap_or("") }
+            })
+            .filter(|sym| sym.t != "U")
+            .collect();
+        let nb_symbol = symbols.len();
+        write!(res_file, "#define FN_DIR_LEN	{}\n", nb_symbol).unwrap();
+        write!(res_file, "static struct symbol_entry function_directory[{}] = {{", nb_symbol,).unwrap();
+        for s in symbols {
+            write!(res_file, "\t{{0x{}, '{}', \"{}\"}},\n", s.addr, s.t, s.name).unwrap();
+        }
+        write!(res_file, "}};\n").unwrap();
     }
-    res_file.write_fmt(format_args!("}};\n")).unwrap();
 }
