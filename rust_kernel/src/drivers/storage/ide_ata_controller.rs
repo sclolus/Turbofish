b24c854fa4406d0cfb113062fab0dbf805a117dd
mordak <bmickael@student.42.fr>
2019-04-19 11:15:01 -0400

    tmp

--- rust_kernel/src/drivers/storage/ide_ata_controller.rs
+++ rust_kernel/src/drivers/storage/ide_ata_controller.rs
@@ -8,3 +8,51 @@ pub use pio_polling::PioPolling;
 
 pub mod pci_udma;
 pub use pci_udma::PciUdma;
+
+use bitflags::bitflags;
+
+/// AtaResult is just made to handle module errors
+pub type AtaResult<T> = core::result::Result<T, AtaError>;
+
+/// Common errors for this module
+#[derive(Debug, Copy, Clone)]
+pub enum AtaError {
+    /// Not a valid position
+    DeviceNotFound,
+    /// Common error Variant
+    NotSupported,
+    /// Out of bound like always
+    OutOfBound,
+    /// There is nothing to do
+    NothingToDo,
+    /// IO error
+    IoError,
+}
+
+// Some errors may occured
+bitflags! {
+    struct ErrorRegister: u8 {
+        const ADDRESS_MARK_NOT_FOUND = 1 << 0;
+        const TRACK_ZERO_NOT_FOUND = 1 << 1;
+        const ABORTED_COMMAND = 1 << 2;
+        const MEDIA_CHANGE_REQUEST = 1 << 3;
+        const ID_MOT_FOUND = 1 << 4;
+        const MEDIA_CHANGED = 1 << 5;
+        const UNCORRECTABLE_DATA_ERROR = 1 << 6;
+        const BAD_BLOCK_DETECTED = 1 << 7;
+    }
+}
+
+// We need always check status register
+bitflags! {
+    struct StatusRegister: u8 {
+        const ERR = 1 << 0; // Indicates an error occurred. Send a new command to clear it (or nuke it with a Software Reset).
+        const IDX = 1 << 1; // Index. Always set to zero.
+        const CORR = 1 << 2; // Corrected data. Always set to zero.
+        const DRQ = 1 << 3; // Set when the drive has PIO data to transfer, or is ready to accept PIO data.
+        const SRV = 1 << 4; // Overlapped Mode Service Request.
+        const DF = 1 << 5; // Drive Fault Error (does not set ERR).
+        const RDY = 1 << 6; // Bit is clear when drive is spun down, or after an error. Set otherwise.
+        const BSY = 1 << 7; //Indicates the drive is preparing to send/receive data (wait for it to clear). In case of 'hang' (it never clears), do a software reset
+    }
+}
