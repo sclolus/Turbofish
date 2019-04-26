0567b31c375c374815335c9fefc20694c0ae9ffe
mordak <bmickael@student.42.fr>
2019-04-26 07:54:10 -0400

    Directory Entry Draft file

--- rust_kernel/userspace_workplace/ext2/src/main.rs
+++ rust_kernel/userspace_workplace/ext2/src/main.rs
@@ -4,7 +4,6 @@
 use bit_field::BitArray;
 
 use core::cmp::min;
-use core::fmt;
 use core::mem::size_of;
 use core::ops::Add;
 
@@ -20,6 +19,9 @@ use header::{BlockGroupDescriptor, SuperBlock};
 
 mod inode;
 use inode::{Inode, TypeAndPerm};
+
+mod directory_entry;
+use directory_entry::{DirectoryEntryHeader};
 
 /// The Ext2 file system divides up disk space into logical blocks of contiguous space.
 /// The size of blocks need not be the same size as the sector size of the disk the file system resides on.
@@ -50,59 +52,6 @@ pub enum IoError {
 
 /// Used to help confirm the presence of Ext2 on a volume
 const EXT2_SIGNATURE_MAGIC: u16 = 0xef53;
-
-#[derive(Debug, Copy, Clone)]
-#[repr(packed)]
-struct DirectoryEntryHeader {
-    /// Inode
-    /*0 	3 	4*/
-    inode: u32,
-    /// Total size of this entry (Including all subfields)
-    /*4 	5 	2*/
-    entry_size: u16,
-    /// Name Length least-significant 8 bits
-    /*6 	6 	1*/
-    name_length: u8,
-    /// Type indicator (only if the feature bit for "directory entries have file type byte" is set, else this is the most-significant 8 bits of the Name Length)
-    /*7 	7 	1*/
-    type_indicator: DirectoryEntryType,
-    /// N 	Name characters
-    /*8 	8+N-1*/
-    filename: Filename,
-}
-
-#[derive(Debug, Copy, Clone)]
-#[allow(dead_code)]
-#[repr(u8)]
-enum DirectoryEntryType {
-    UnknownType,
-    RegularFile,
-    Directory,
-    CharacterDevice,
-    BlockDevice,
-    Fifo,
-    Socket,
-    SymbolicLink,
-}
-
-impl DirectoryEntryHeader {
-    pub fn get_filename(&self) -> &str {
-        unsafe {
-            let slice: &[u8] = core::slice::from_raw_parts(&self.filename.0 as *const u8, self.name_length as usize);
-            core::str::from_utf8_unchecked(slice)
-        }
-    }
-}
-
-#[derive(Copy, Clone)]
-#[repr(transparent)]
-pub struct Filename(pub [u8; 256]);
-
-impl fmt::Debug for Filename {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", "Filename")
-    }
-}
 
 struct EntryIter<'a> {
     filesystem: &'a mut Ext2Filesystem,
