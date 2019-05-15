5ac76cfa0a6ad1b1de7de250732c3101635088ab
mordak <bmickael@student.42.fr>
2019-05-14 20:48:38 -0400

    syscall sanitize A future factorization is necessary

--- rust_kernel/src/syscall/syscall_isr.asm
+++ rust_kernel/src/syscall/syscall_isr.asm
@@ -4,44 +4,34 @@ extern syscall_interrupt_handler
 extern kernel_stack
 
 segment .data
-_pic_time dd 0
-_OLD_EIP:	dd 0
-_OLD_EAX:	dd 0
-_OLD_ESP:	dd 0
-_OLD_EFLAGS:	dd 0
-_OLD_SEGMENT:	dd 0
+_eip: dd 0
+_eax: dd 0
+_esp: dd 0
+_eflags: dd 0
+_cs: dd 0
 
 segment .text
 global _isr_syscall
 
 _isr_syscall:
     ; already on stack: ss, sp, flags, cs, ip.
-	mov [_OLD_EAX], eax
-	pop eax
-	mov [_OLD_EIP], eax
-	pop eax
-	mov [_OLD_SEGMENT], eax
-	pop eax
-	mov [_OLD_EFLAGS], eax
+	pop dword [_eip]
+	pop dword [_cs]
+	pop dword [_eflags]
 
+	; Save the process stack and change stack to kernel stack
+	mov [_eax], eax
 	mov eax, esp
-	mov [_OLD_ESP], eax
+	mov [_esp], eax
 	mov esp, kernel_stack
+	mov eax, [_eax]
 
-	mov eax, [_OLD_EAX]
+	; Push all the process purpose registers
 	pushad
-
-	mov eax, [_OLD_ESP]
-	push eax
-
-	mov eax, [_OLD_EFLAGS]
-	push eax
-
-	mov eax, [_OLD_SEGMENT]
-	push eax
-
-	mov eax, [_OLD_EIP]
-	push eax
+	push dword [_esp]
+	push dword [_eflags]
+	push dword [_cs]
+	push dword [_eip]
 
 	call syscall_interrupt_handler
 	; no return
