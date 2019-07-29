cadbf8595be68e464fe6edf13455c790e361c1fd
vcombey <vcombey@student.42.fr>
2019-07-29 21:45:09 +0200

    setjmp longjmp

--- programs/libc/include/setjmp.h
+++ programs/libc/include/setjmp.h
@@ -2,8 +2,8 @@
 # define __SETJMP_H__
 //[CX] [Option Start] Some of the functionality described on this reference page extends the ISO C standard. Applications shall define the appropriate feature test macro (see XSH The Compilation Environment ) to enable the visibility of these symbols in this header. [Option End]
 
-typedef int jmp_buf [42];
-typedef  int sigjmp_buf[42];
+typedef int jmp_buf[24];
+typedef int sigjmp_buf[24];
 //The <setjmp.h> header shall define the array types jmp_buf and [CX] [Option Start] sigjmp_buf. [Option End]
 
 //The following shall be declared as functions and may also be defined as macros. Function prototypes shall be provided.
