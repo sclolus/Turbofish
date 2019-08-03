7882dc539fbcc306c3fff447418fd7ab135c9443
mordak <bmickael@student.42.fr>
2019-08-03 16:33:00 -0400

    Addition of sigismember fn into libc

---
+++ programs/libc/src/signal/sigismember.c
@@ -0,0 +1,11 @@
+
+#include <signal.h>
+#include <errno.h>
+
+int    sigismember(const sigset_t *set, int signo) {
+	if (signo < 0 || signo > 31) {
+		errno = EINVAL;
+		return -1;
+	}
+	return (*set & (1 << signo)) != 0;
+}
