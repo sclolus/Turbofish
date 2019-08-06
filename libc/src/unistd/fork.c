bca834dad39df723f518ab72b4a5ea8307d288d4
mordak <bmickael@student.42.fr>
2019-08-06 04:07:48 -0400

    Use of legacy fork in libc side when fork function is invoqued

--- libc/src/unistd/fork.c
+++ libc/src/unistd/fork.c
@@ -1,20 +1,18 @@
-#include <sched.h>
 #include <user_syscall.h>
 #include <unistd.h>
 #include <errno.h>
-
-extern int errno;
 
 /*
  * fork - create a child process
  */
 pid_t fork()
 {
-	pid_t ret = _user_syscall(CLONE, 2, NULL, 0 /*CLONE_CHILD_CLEARTID|CLONE_CHILD_SETTID|SIGCHLD*/);
+	int ret = _user_syscall(FORK, 0);
 	/*
 	 * On success, the PID of the child process is returned in the parent,
 	 * and 0 is returned in the child.  On failure, -1 is returned in the
 	 * parent, no child process is created, and errno is set appropriately.
 	 */
+
 	set_errno_and_return(ret);
 }
