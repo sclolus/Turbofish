bdf5182f53f43597e64f32aa1e28650a426af272
vcombey <vcombey@student.42.fr>
2019-08-08 17:02:14 +0200

    signal with an enum

--- programs/libc/include/signal.h
+++ programs/libc/include/signal.h
@@ -2,43 +2,45 @@
 # define __SIGNAL_H__
 
 /* Signals.  */
-#define SIGNULL        0       /* NULL signal for logical raisons */
+enum Signum {
+	SIGNULL     =  0 ,     /* NULL signal for logical raisons */
 
-#define SIGHUP         1       /* Hangup (POSIX).  */
-#define SIGINT         2       /* Interrupt (ANSI).  */
-#define SIGQUIT        3       /* Quit (POSIX).  */
-#define SIGILL         4       /* Illegal instruction (ANSI).  */
-#define SIGTRAP        5       /* Trace trap (POSIX).  */
-#define SIGABRT        6       /* Abort (ANSI).  */
-#define SIGIOT         6       /* IOT trap (4.2 BSD).  */
-#define SIGBUS         7       /* BUS error (4.2 BSD).  */
-#define SIGFPE         8       /* Floating-point exception (ANSI).  */
-#define SIGKILL        9       /* Kill, unblockable (POSIX).  */
-#define SIGUSR1        10      /* User-defined signal 1 (POSIX).  */
-#define SIGSEGV        11      /* Segmentation violation (ANSI).  */
-#define SIGUSR2        12      /* User-defined signal 2 (POSIX).  */
-#define SIGPIPE        13      /* Broken pipe (POSIX).  */
-#define SIGALRM        14      /* Alarm clock (POSIX).  */
-#define SIGTERM        15      /* Termination (ANSI).  */
-#define SIGSTKFLT      16      /* Stack fault.  */
-#define SIGCLD         SIGCHLD /* Same as SIGCHLD (System V).  */
-#define SIGCHLD        17      /* Child status has changed (POSIX).  */
-#define SIGCONT        18      /* Continue (POSIX).  */
-#define SIGSTOP        19      /* Stop, unblockable (POSIX).  */
-#define SIGTSTP        20      /* Keyboard stop (POSIX).  */
-#define SIGTTIN        21      /* Background read from tty (POSIX).  */
-#define SIGTTOU        22      /* Background write to tty (POSIX).  */
-#define SIGURG         23      /* Urgent condition on socket (4.2 BSD).  */
-#define SIGXCPU        24      /* CPU limit exceeded (4.2 BSD).  */
-#define SIGXFSZ        25      /* File size limit exceeded (4.2 BSD).  */
-#define SIGVTALRM      26      /* Virtual alarm clock (4.2 BSD).  */
-#define SIGPROF        27      /* Profiling alarm clock (4.2 BSD).  */
-#define SIGWINCH       28      /* Window size change (4.3 BSD, Sun).  */
-#define SIGPOLL        SIGIO   /* Pollable event occurred (System V).  */
-#define SIGIO          29      /* I/O now possible (4.2 BSD).  */
-#define SIGPWR         30      /* Power failure restart (System V).  */
-#define SIGSYS         31      /* Bad system call.  */
-#define SIGUNUSED      31
+	SIGHUP      =  1 ,     /* Hangup (POSIX).  */
+	SIGINT      =  2 ,     /* Interrupt (ANSI).  */
+	SIGQUIT     =  3 ,     /* Quit (POSIX).  */
+	SIGILL      =  4 ,     /* Illegal instruction (ANSI).  */
+	SIGTRAP     =  5 ,     /* Trace trap (POSIX).  */
+	SIGABRT     =  6 ,     /* Abort (ANSI).  */
+	SIGIOT      =  6 ,     /* IOT trap (4.2 BSD).  */
+	SIGBUS      =  7 ,     /* BUS error (4.2 BSD).  */
+	SIGFPE      =  8 ,     /* Floating-point exception (ANSI).  */
+	SIGKILL     =  9 ,     /* Kill, unblockable (POSIX).  */
+	SIGUSR1     =  10,     /* User-defined signal 1 (POSIX).  */
+	SIGSEGV     =  11,     /* Segmentation violation (ANSI).  */
+	SIGUSR2     =  12,     /* User-defined signal 2 (POSIX).  */
+	SIGPIPE     =  13,     /* Broken pipe (POSIX).  */
+	SIGALRM     =  14,     /* Alarm clock (POSIX).  */
+	SIGTERM     =  15,     /* Termination (ANSI).  */
+	SIGSTKFLT   =  16,     /* Stack fault.  */
+	SIGCHLD     =  17,     /* Child status has changed (POSIX).  */
+	SIGCLD      =  SIGCHLD,/* Same as SIGCHLD (System V).  */
+	SIGCONT     =  18,     /* Continue (POSIX).  */
+	SIGSTOP     =  19,     /* Stop, unblockable (POSIX).  */
+	SIGTSTP     =  20,     /* Keyboard stop (POSIX).  */
+	SIGTTIN     =  21,     /* Background read from tty (POSIX).  */
+	SIGTTOU     =  22,     /* Background write to tty (POSIX).  */
+	SIGURG      =  23,     /* Urgent condition on socket (4.2 BSD).  */
+	SIGXCPU     =  24,     /* CPU limit exceeded (4.2 BSD).  */
+	SIGXFSZ     =  25,     /* File size limit exceeded (4.2 BSD).  */
+	SIGVTALRM   =  26,     /* Virtual alarm clock (4.2 BSD).  */
+	SIGPROF     =  27,     /* Profiling alarm clock (4.2 BSD).  */
+	SIGWINCH    =  28,     /* Window size change (4.3 BSD, Sun).  */
+	SIGIO       =  29,     /* I/O now possible (4.2 BSD).  */
+	SIGPOLL     =  SIGIO,  /* Pollable event occurred (System V).  */
+	SIGPWR      =  30,     /* Power failure restart (System V).  */
+	SIGSYS      =  31,     /* Bad system call.  */
+	SIGUNUSED   =  31
+};
 
 typedef void (*sighandler_t)(int);
 
