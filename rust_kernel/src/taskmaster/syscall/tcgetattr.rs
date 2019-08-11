9f67cda9ba1586881c600fa55d8bc389b17ce4d5
vcombey <vcombey@student.42.fr>
2019-08-11 14:34:20 +0200

    add doc on syscall

--- rust_kernel/src/taskmaster/syscall/tcgetattr.rs
+++ rust_kernel/src/taskmaster/syscall/tcgetattr.rs
@@ -2,6 +2,14 @@ use super::SysResult;
 use crate::terminal::TERMINAL;
 use libc_binding::termios;
 
+/// The tcgetattr() function shall get the parameters associated with
+/// the terminal referred to by fildes and store them in the termios
+/// structure referenced by termios_p. The fildes argument is an open
+/// file descriptor associated with a terminal.
+///
+/// The termios_p argument is a pointer to a termios structure.
+///
+/// The tcgetattr() operation is allowed from any process.
 pub fn sys_tcgetattr(_fildes: i32, termios_p: *mut termios) -> SysResult<u32> {
     unpreemptible_context!({
         // TODO: change this 1
--- rust_kernel/src/taskmaster/syscall/tcsetpgrp.rs
+++ rust_kernel/src/taskmaster/syscall/tcsetpgrp.rs
@@ -2,6 +2,21 @@ use super::Pid;
 use super::SysResult;
 use crate::terminal::TERMINAL;
 
+/// If the process has a controlling terminal, tcsetpgrp() shall set
+/// the foreground process group ID associated with the terminal to
+/// pgid_id. The application shall ensure that the file associated
+/// with fildes is the controlling terminal of the calling process and
+/// the controlling terminal is currently associated with the session
+/// of the calling process. The application shall ensure that the
+/// value of pgid_id matches a process group ID of a process in the
+/// same session as the calling process.
+///
+/// Attempts to use tcsetpgrp() from a process which is a member of a
+/// background process group on a fildes associated with its
+/// controlling terminal shall cause the process group to be sent a
+/// SIGTTOU signal. If the calling thread is blocking SIGTTOU signals
+/// or the process is ignoring SIGTTOU signals, the process shall be
+/// allowed to perform the operation, and no signal is sent.
 pub fn sys_tcsetpgrp(_fildes: i32, pgid_id: Pid) -> SysResult<u32> {
     unpreemptible_context!({
         unsafe {
