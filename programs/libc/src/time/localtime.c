d63112d99245552d9599559d64ab4ecf2b809d6a
mordak <bmickael@student.42.fr>
2019-08-16 19:58:43 -0400

    Fix ugly style

--- programs/libc/src/time/localtime.c
+++ programs/libc/src/time/localtime.c
@@ -40,7 +40,7 @@
 /// daylight and timezone. If localtime_r() does not set tzname, it
 /// shall not set daylight and shall not set timezone. [Option End]
 
-///URN VALUE
+/// URN VALUE
 
 /// Upon successful completion, the localtime() function shall return
 /// a pointer to the broken-down time structure. If an error is
@@ -54,14 +54,14 @@ static struct tm TM;
 struct tm *localtime(const time_t *timer)
 {
 	(void)timer;
-	TM.tm_sec   = 0;// Seconds [0,60].
-	TM.tm_min   = 0;// Minutes [0,59].
-	TM.tm_hour  = 0;// Hour [0,23].
-	TM.tm_mday  = 1;// Day of month [1,31].
-	TM.tm_mon   = 0;// Month of year [0,11].
-	TM.tm_year  = 0;// Years since 1900.
-	TM.tm_wday  = 0;// Day of week [0,6] (Sunday =0).
-	TM.tm_yday  = 0;// Day of year [0,365].
-	TM.tm_isdst = 0;// Daylight Savings flag.
+	TM.tm_sec   = 0; // Seconds [0,60].
+	TM.tm_min   = 0; // Minutes [0,59].
+	TM.tm_hour  = 0; // Hour [0,23].
+	TM.tm_mday  = 1; // Day of month [1,31].
+	TM.tm_mon   = 0; // Month of year [0,11].
+	TM.tm_year  = 0; // Years since 1900.
+	TM.tm_wday  = 0; // Day of week [0,6] (Sunday =0).
+	TM.tm_yday  = 0; // Day of year [0,365].
+	TM.tm_isdst = 0; // Daylight Savings flag.
 	return &TM;
 }
