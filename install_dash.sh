1f97390ba8fcef23b148a7b0f2d1f13bb4dbfc2e
vcombey <vcombey@student.42.fr>
2019-08-18 00:09:55 +0200

    build dash le retour

--- install_dash.sh
+++ install_dash.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export TARGET="i686-turbofish"
 export PATH="/toolchain_turbofish/cross/bin:$PATH"
+export TARGET_DIR="../../../rust_kernel/src/userland"
 
 mkdir -pv build_dash
 cd build_dash
@@ -9,5 +10,6 @@ tar -xf 'dash-0.5.10.tar.gz'
 cd dash-0.5.10
 mkdir build
 cd build
-../configure --host=$TARGET
+CFLAGS="-g -O0 -fno-omit-frame-pointer" ../configure --host=$TARGET
 make
+cp -v src/dash $TARGET_DIR
--- programs/libc/include/sys/stat.h
+++ programs/libc/include/sys/stat.h
@@ -37,6 +37,39 @@ struct stat {
 
 };
 
+//TODO: Dash need that to compile, check that
+struct stat64 {
+	dev_t st_dev;            // Device ID of device containing file. 
+	ino_t st_ino;            // File serial number. 
+	mode_t st_mode;          // Mode of file (see below). 
+	nlink_t st_nlink;        // Number of hard links to the file. 
+	uid_t st_uid;            // User ID of file. 
+	gid_t st_gid;            // Group ID of file. 
+	//[XSI][Option Start]
+	dev_t st_rdev;           // Device ID (if file is character or block special). 
+	//[Option End]
+	off_t st_size;           // For regular files, the file size in bytes. 
+	// For symbolic links, the length in bytes of the 
+	// pathname contained in the symbolic link. 
+	//[SHM][Option Start]
+	// For a shared memory object, the length in bytes. 
+	//[Option End]
+	//[TYM][Option Start]
+	// For a typed memory object, the length in bytes. 
+	//[Option End]
+	// For other file types, the use of this field is 
+	// unspecified. 
+	struct timespec st_atim; // Last data access timestamp. 
+	struct timespec st_mtim; // Last data modification timestamp. 
+	struct timespec st_ctim; // Last file status change timestamp. 
+	//[XSI][Option Start]
+	blksize_t st_blksize;    // A file system-specific preferred I/O block size 
+	// for this object. In some file system types, this 
+	// may vary from file to file. 
+	blkcnt_t st_blocks;      // Number of blocks allocated for this object. 
+	//[Option End]
+};
+
 #define st_atime st_atim.tv_sec      /* Backward compatibility */
 #define st_mtime st_mtim.tv_sec
 #define st_ctime st_ctim.tv_sec
