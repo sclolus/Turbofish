4011ec5b23978c23685f647e491640b4c4aa91f0
vcombey <vcombey@student.42.fr>
2019-08-24 15:26:21 +0200

    try fix libc_binding

--- rust_kernel/dependencies/libc_binding/build.rs
+++ rust_kernel/dependencies/libc_binding/build.rs
@@ -3,6 +3,6 @@ use std::process::Command;
 fn main() {
     let out = Command::new("make").output().unwrap();
     if !out.status.success() {
-        panic!("{:?}", String::from_utf8(out.stderr));
+        panic!("{:?}", out);
     }
 }
