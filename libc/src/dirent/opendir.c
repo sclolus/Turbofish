929196da01c437230ceca21292cf990c4916500d
mordak <bmickael@student.42.fr>
2019-09-01 20:17:04 -0400

    Create closedir, opendir && readdir boilerplates on libcside

--- libc/src/dirent/opendir.c
+++ libc/src/dirent/opendir.c
@@ -1,6 +1,7 @@
 #include <dirent.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <user_syscall.h>
 
 // The opendir() function shall open a directory stream corresponding
 // to the directory named by the dirname argument. The directory
@@ -8,13 +9,22 @@
 // implemented using a file descriptor, applications shall only be
 // able to open up to a total of {OPEN_MAX} files and directories.
 
-#warning NOT IMPLEMENTED
-#include <custom.h>
-
 DIR *opendir(const char *dirname)
 {
-	DUMMY
-	(void)dirname;
-	errno = ENOSYS;
-	return NULL;
+	DIR *dir = (DIR *)malloc(sizeof(DIR));
+	if (dir == NULL) {
+		return NULL;
+	}
+	int ret = _user_syscall(OPENDIR, 2, dirname, dir);
+	/*
+	 * The opendir() and fdopendir() functions return a pointer to the directory stream.
+	 * On error, NULL is returned, and errno is set appropriately.
+	 */
+	if (ret < 0) {
+		errno = -ret;
+		free(dir);
+		return NULL;
+	} else {
+		return dir;
+	}
 }
