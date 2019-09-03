1c666f2cb81cc7fa0007f6b52be2bb2fec5109b3
vcombey <vcombey@student.42.fr>
2019-09-03 02:11:29 +0200

    trace syscall do log::info

--- rust_kernel/src/taskmaster/syscall/trace_syscall.rs
+++ rust_kernel/src/taskmaster/syscall/trace_syscall.rs
@@ -35,108 +35,128 @@ pub fn trace_syscall(cpu_state: *mut CpuState) {
     } = unsafe { (*cpu_state).registers };
     unpreemptible_context!({
         match eax {
-            EXIT => eprintln!("exit({:#?})", ebx as i32),
-            FORK => eprintln!("fork()"),
-            READ => eprintln!(
+            EXIT => log::info!("exit({:#?})", ebx as i32),
+            FORK => log::info!("fork()"),
+            READ => log::info!(
                 "read({:#?}, {:#?}, {:#?})",
-                ebx as i32, ecx as *mut u8, edx as usize
+                ebx as i32,
+                ecx as *mut u8,
+                edx as usize
             ),
-            WRITE => eprintln!(
+            WRITE => log::info!(
                 "write({:#?}, {:#?}, {:#?})",
-                ebx as i32, ecx as *const u8, edx as usize
+                ebx as i32,
+                ecx as *const u8,
+                edx as usize
             ),
             // TODO: type parameter are not set and manage the third argument
-            OPEN => eprintln!("open({:#?}, {:#?})", ebx as *const u8, ecx as u32),
-            CLOSE => eprintln!("close({:#?})", ebx as i32),
-            WAITPID => eprintln!(
+            OPEN => log::info!("open({:#?}, {:#?})", ebx as *const u8, ecx as u32),
+            CLOSE => log::info!("close({:#?})", ebx as i32),
+            WAITPID => log::info!(
                 "waitpid({:#?}, {:#?}, {:#?})",
-                ebx as i32, ecx as *mut i32, edx as i32
+                ebx as i32,
+                ecx as *mut i32,
+                edx as i32
             ),
-            UNLINK => eprintln!("unlink({:#?})", ebx as *const u8),
-            EXECVE => eprintln!(
+            UNLINK => log::info!("unlink({:#?})", ebx as *const u8),
+            EXECVE => log::info!(
                 "execve({:#?}, {:#?}, {:#?})",
-                ebx as *const c_char, ecx as *const *const c_char, edx as *const *const c_char,
+                ebx as *const c_char,
+                ecx as *const *const c_char,
+                edx as *const *const c_char,
             ),
-            STAT => eprintln!(
+            STAT => log::info!(
                 "stat(filename: {:?}, buf: {:#X?})",
-                ebx as *const c_char, ecx as *mut stat
+                ebx as *const c_char,
+                ecx as *mut stat
             ),
-            LSEEK => eprintln!(
+            LSEEK => log::info!(
                 "lseek(ptr: {:#?}, fd: {:#?}, offset: {:#?}, whence_value: {:#?})",
                 ebx as *mut off_t,
                 ecx as Fd,
                 edx as off_t + ((esi as off_t) << 32),
                 edi as u32,
             ),
-            GETPID => eprintln!("getpid()"),
-            SETUID => eprintln!("setuid({:#?})", ebx as uid_t),
-            GETUID => eprintln!("getuid()"),
-            PAUSE => eprintln!("pause()"),
-            FSTAT => eprintln!("fstat(fd: {:?}, buf: {:#X?})", ebx as Fd, ecx as *mut stat),
-            KILL => eprintln!("kill({:#?}, {:#?})", ebx as i32, ecx as u32),
-            PIPE => eprintln!("pipe({:#?})", ebx as *const i32),
-            DUP => eprintln!("dup({:#?})", ebx as u32),
-            SETGID => eprintln!("setgid({:#?})", ebx as gid_t),
-            GETGID => eprintln!("getgid()"),
-            GETEUID => eprintln!("geteuid()"),
-            FCNTL => eprintln!(
+            GETPID => log::info!("getpid()"),
+            SETUID => log::info!("setuid({:#?})", ebx as uid_t),
+            GETUID => log::info!("getuid()"),
+            PAUSE => log::info!("pause()"),
+            FSTAT => log::info!("fstat(fd: {:?}, buf: {:#X?})", ebx as Fd, ecx as *mut stat),
+            KILL => log::info!("kill({:#?}, {:#?})", ebx as i32, ecx as u32),
+            PIPE => log::info!("pipe({:#?})", ebx as *const i32),
+            DUP => log::info!("dup({:#?})", ebx as u32),
+            SETGID => log::info!("setgid({:#?})", ebx as gid_t),
+            GETGID => log::info!("getgid()"),
+            GETEUID => log::info!("geteuid()"),
+            FCNTL => log::info!(
                 "fcntl({:#?}, {:#?}, {:#?})",
-                ebx as Fd, ecx as u32, edx as Fd
+                ebx as Fd,
+                ecx as u32,
+                edx as Fd
             ),
-            GETEGID => eprintln!("getegid()"),
-            SIGNAL => eprintln!("signal({:#?}, {:#?})", ebx as u32, ecx as usize),
-            SETPGID => eprintln!("setpgid({:#?}, {:#?})", ebx as Pid, ecx as Pid),
-            GETPPID => eprintln!("getppid()"),
-            DUP2 => eprintln!("dup2({:#?}, {:#?})", ebx as u32, ecx as u32),
-            GETPGRP => eprintln!("getpgrp()"),
-            SIGACTION => eprintln!(
+            GETEGID => log::info!("getegid()"),
+            SIGNAL => log::info!("signal({:#?}, {:#?})", ebx as u32, ecx as usize),
+            SETPGID => log::info!("setpgid({:#?}, {:#?})", ebx as Pid, ecx as Pid),
+            GETPPID => log::info!("getppid()"),
+            DUP2 => log::info!("dup2({:#?}, {:#?})", ebx as u32, ecx as u32),
+            GETPGRP => log::info!("getpgrp()"),
+            SIGACTION => log::info!(
                 "sigaction({:#?}, {:#?}, {:#?})",
-                ebx as u32, ecx as *const StructSigaction, edx as *mut StructSigaction,
+                ebx as u32,
+                ecx as *const StructSigaction,
+                edx as *mut StructSigaction,
             ),
-            SIGSUSPEND => eprintln!("sigsuspend({:#?})", ebx as *const sigset_t),
-            GETGROUPS => eprintln!("getgroups({:#?}, {:#?})", ebx as i32, ecx as *mut gid_t),
-            SETGROUPS => eprintln!("setgroups({:#?}, {:#?})", ebx as i32, ecx as *const gid_t),
-            LSTAT => eprintln!("lstat(fd: {:?}, buf: {:#X?})", ebx as Fd, ecx as *mut stat),
-            REBOOT => eprintln!("reboot()"),
-            MMAP => unsafe { eprintln!("mmap({:#?})", *(ebx as *const MmapArgStruct)) },
-            MUNMAP => eprintln!("munmap({:#?}, {:#?})", Virt(ebx as usize), ecx as usize),
-            SOCKETCALL => eprintln!("socketcall({:#?}, {:#?})", ebx as u32, ecx as SocketArgsPtr),
-            CLONE => eprintln!(
+            SIGSUSPEND => log::info!("sigsuspend({:#?})", ebx as *const sigset_t),
+            GETGROUPS => log::info!("getgroups({:#?}, {:#?})", ebx as i32, ecx as *mut gid_t),
+            SETGROUPS => log::info!("setgroups({:#?}, {:#?})", ebx as i32, ecx as *const gid_t),
+            LSTAT => log::info!("lstat(fd: {:?}, buf: {:#X?})", ebx as Fd, ecx as *mut stat),
+            REBOOT => log::info!("reboot()"),
+            MMAP => unsafe { log::info!("mmap({:#?})", *(ebx as *const MmapArgStruct)) },
+            MUNMAP => log::info!("munmap({:#?}, {:#?})", Virt(ebx as usize), ecx as usize),
+            SOCKETCALL => log::info!("socketcall({:#?}, {:#?})", ebx as u32, ecx as SocketArgsPtr),
+            CLONE => log::info!(
                 "clone({:#?}, {:#?}, {:#?})",
-                cpu_state as u32, ebx as *const c_void, ecx as u32
+                cpu_state as u32,
+                ebx as *const c_void,
+                ecx as u32
             ),
-            MPROTECT => eprintln!(
+            MPROTECT => log::info!(
                 "mprotect({:#?}, {:#?}, {:#?})",
                 Virt(ebx as usize),
                 ecx as usize,
                 MmapProt::from_bits_truncate(edx),
             ),
-            SIGPROCMASK => eprintln!(
+            SIGPROCMASK => log::info!(
                 "sigprocmask({:#?}, {:#?}, {:#?})",
-                ebx as i32, ecx as *const sigset_t, edx as *mut sigset_t
+                ebx as i32,
+                ecx as *const sigset_t,
+                edx as *mut sigset_t
             ),
-            GETPGID => eprintln!("getpgid({:#?})", ebx as Pid),
-            NANOSLEEP => eprintln!(
+            GETPGID => log::info!("getpgid({:#?})", ebx as Pid),
+            NANOSLEEP => log::info!(
                 "nanosleep({:#?}, {:#?})",
-                ebx as *const TimeSpec, ecx as *mut TimeSpec
+                ebx as *const TimeSpec,
+                ecx as *mut TimeSpec
             ),
-            SIGRETURN => eprintln!("sigreturn({:#?})", cpu_state),
-            SHUTDOWN => eprintln!("shutdown()"),
-            TEST => eprintln!("test()"),
-            STACK_OVERFLOW => eprintln!("stack_overflow()"),
-            EXIT_QEMU => eprintln!("exit_qemu({:#?})", ebx as u32),
-            TCGETATTR => eprintln!("tcgetattr({:#?}, {:#?})", ebx as i32, ecx as *mut termios),
-            TCSETATTR => eprintln!(
+            SIGRETURN => log::info!("sigreturn({:#?})", cpu_state),
+            SHUTDOWN => log::info!("shutdown()"),
+            TEST => log::info!("test()"),
+            STACK_OVERFLOW => log::info!("stack_overflow()"),
+            EXIT_QEMU => log::info!("exit_qemu({:#?})", ebx as u32),
+            TCGETATTR => log::info!("tcgetattr({:#?}, {:#?})", ebx as i32, ecx as *mut termios),
+            TCSETATTR => log::info!(
                 "tcsetattr({:#?}, {:#?}, {:#?})",
-                ebx as i32, ecx as u32, edx as *const termios
+                ebx as i32,
+                ecx as u32,
+                edx as *const termios
             ),
-            TCSETPGRP => eprintln!("tcsetpgrp({:#?}, {:#?})", ebx as i32, ecx as Pid),
-            TCGETPGRP => eprintln!("tcgetpgrp({:#?})", ebx as i32),
-            SETEGID => eprintln!("setegid({:#?})", ebx as gid_t),
-            SETEUID => eprintln!("seteuid({:#?})", ebx as uid_t),
-            ISATTY => eprintln!("isatty({:#?})", ebx as u32),
-            OPENDIR => eprintln!("opendir({:#?}, {:#?})", ebx as *const u8, ecx as *mut DIR),
-            _ => eprintln!("unknown syscall()",),
+            TCSETPGRP => log::info!("tcsetpgrp({:#?}, {:#?})", ebx as i32, ecx as Pid),
+            TCGETPGRP => log::info!("tcgetpgrp({:#?})", ebx as i32),
+            SETEGID => log::info!("setegid({:#?})", ebx as gid_t),
+            SETEUID => log::info!("seteuid({:#?})", ebx as uid_t),
+            ISATTY => log::info!("isatty({:#?})", ebx as u32),
+            OPENDIR => log::info!("opendir({:#?}, {:#?})", ebx as *const u8, ecx as *mut DIR),
+            _ => log::info!("unknown syscall()",),
         }
     })
 }
@@ -201,6 +221,6 @@ pub fn trace_syscall_result(cpu_state: *mut CpuState, result: SysResult<u32>) {
         _ => "unknown syscall",
     };
     unpreemptible_context!({
-        eprintln!("{} result: {:#?}", sysname, result);
+        log::info!("{} result: {:#?}", sysname, result);
     })
 }
