bf881c08516892d37296e6adad1cab88bb9d63b9
miniske <miniske@Debian-Seb-PC.Debian-Seb-PC>
2019-09-06 16:37:48 +0200

    Implemented getchar and getchar_unlocked

---
+++ libc/src/stdio/getchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <errno.h>
+
+/* The getchar() function shall be equivalent to getc(stdin). */
+
+int      getchar(void)
+{
+	return getc(stdin);
+}
+
+int      getchar_unlocked(void);
+{
+	return getchar();
+}
