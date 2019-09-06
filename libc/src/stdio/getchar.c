b98743974a5dbb7daed76cf75444ef6ebb809fe2
miniske <miniske@Debian-Seb-PC.Debian-Seb-PC>
2019-09-06 16:57:54 +0200

    Put getchar_unlocked in an individual file

--- libc/src/stdio/getchar.c
+++ libc/src/stdio/getchar.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
-#include <errno.h>
 
 /* The getchar() function shall be equivalent to getc(stdin). */
 
 int      getchar(void)
 {
 	return getc(stdin);
-}
-
-int      getchar_unlocked(void)
-{
-	return getchar();
 }
