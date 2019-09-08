d0ddfabfb72d69bb32b6476a9f388fa8ce59c03c
vcombey <vcombey@student.42.fr>
2019-09-09 01:42:29 +0200

    add test unlink orphan

---
+++ programs/tests/src/unlink/unlink_orphan.c
@@ -0,0 +1,43 @@
+#include <unistd.h>
+#include <limits.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+	char filename[100];
+
+	pid_t pid = getpid();
+	sprintf(filename, "./file_%d", pid);
+
+	printf("creating file: %s\n", filename);
+	int fd = open(filename, O_RDWR | O_CREAT, 0644);
+	if (fd == -1) {
+		perror("open");
+		exit(1);
+	}
+	int ret = unlink(filename);
+	if (ret == -1) {
+		perror("unlink");
+		exit(1);
+	}
+
+	char s[] = "banane";
+	ret = write(fd, s, sizeof(s));
+	if (ret == -1) {
+		perror("write");
+		exit(1);
+	}
+
+	char r[sizeof(s)];
+	ret = read(fd, r, sizeof(s));
+	if (ret == -1) {
+		perror("read");
+		exit(1);
+	}
+	if (strcmp(r, s) != 0) {
+		printf("error data writen != data read\n");
+	}
+	close(fd);
+}
