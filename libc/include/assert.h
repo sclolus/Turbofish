3cc1de6ed2204f94de643ce6112a4c7b511e4620
miniske <miniske@Debian-Seb-PC.Debian-Seb-PC>
2019-09-08 09:56:18 +0200

    Fixed retarded gcc warning on assert definition

--- libc/include/assert.h
+++ libc/include/assert.h
@@ -2,6 +2,7 @@
 # define __ASSERT_H__
 
 # include <stdio.h> //eeeeeeh.
+# include <stdlib.h>
 __attribute__((noreturn, always_inline)) inline static void	__assertion_failure(const char *assertion, const char *file, unsigned int line)
 {
 	dprintf(2, "assertion failed(%s:%u): %s\n", file, line, assertion);
@@ -9,6 +10,6 @@ __attribute__((noreturn, always_inline)) inline static void	__assertion_failure(
 
 }
 
-# define assert(assertion) (assertion ?: __assertion_failure(#assertion, __FILE__, __LINE__))
+# define assert(assertion) ((assertion) ? (void)0 : __assertion_failure(#assertion, __FILE__, __LINE__))
 
 #endif  /* __ASSERT_H__ */
