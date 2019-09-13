38343284b188a74883708b88050d2c61c070887a
vcombey <vcombey@student.42.fr>
2019-09-13 03:22:11 +0200

    remove dash dependency in execl test

--- programs/tests/src/execl/execl.c
+++ programs/tests/src/execl/execl.c
@@ -3,17 +3,30 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <errno.h>
+#include <string.h>
 
 // This binary shall execute dash, that shall execute the `env` binary and sucessfully exit.
 // The env binary should print the EXCL_TEST variable with a value of `working`.
-int main(void)
+int main(int ac, char **av)
 {
 	const char  *key = "EXCL_TEST";
 	const char  *value = "working";
 
+	char *previous_env = getenv(key);
+	if (previous_env != NULL) {
+		printf("%s\n", previous_env);
+	} else {
+		printf("NULL\n");
+	}
+	if (previous_env != NULL && strcmp(previous_env, value) == 0) {
+		assert(ac == 2);
+		assert(strcmp(av[0], "a") == 0);
+		assert(strcmp(av[1], "b") == 0);
+		return EXIT_SUCCESS;
+	}
 	assert(setenv(key, value, 1) == 0);
 	printf("Set '%s' env variable to: '%s'\n", key, value);
-	int ret = execl("/bin/dash", "dash", "-c", "/bin/env", (char*)NULL);
+	int ret = execl("/bin/execl/execl", "a", "b", (char*)NULL);
 
 	assert(ret == -1);
 	perror("Execl failed with: ");
