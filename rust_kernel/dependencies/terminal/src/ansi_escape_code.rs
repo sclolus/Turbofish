2b29f5ec9d23b585b5ed4c7124d582b85d3cd262
vcombey <vcombey@student.42.fr>
2019-09-25 16:10:28 +0200

    fix last escape bug

--- rust_kernel/dependencies/terminal/src/ansi_escape_code.rs
+++ rust_kernel/dependencies/terminal/src/ansi_escape_code.rs
@@ -53,40 +53,36 @@ impl<'a> Iterator for IterEscaped<'a> {
         if self.off >= self.s.len() {
             return None;
         }
-        assert_str_is_valid(&self.s[self.off..]);
         Some(if self.s[self.off..].starts_with(0x1b as char) {
-            assert!(self.s[self.off..].len() != 0);
-            assert!(self.s.is_char_boundary(self.off));
-            assert_str_is_valid(&self.s[self.off..]);
-            let next_alpha = self.off
-                + self.s[self.off..]
-                    .find(|x: char| x.is_ascii_alphabetic())
-                    .unwrap_or(self.s[self.off..].len() - 1);
-            assert!(next_alpha < self.s.len());
-            let ret = &self.s[self.off..=next_alpha];
-            assert_str_is_valid(ret);
-            self.off = next_alpha + 1;
-            assert_str_is_valid(&self.s[next_alpha..next_alpha + 1]);
-            if &self.s[next_alpha..next_alpha + 1] == "m" {
-                match AnsiColor::from_str(ret) {
-                    Ok(c) => Escaped(EscapedCode::Color(c)),
-                    Err(_) => Str(ret),
+            match self.s[self.off..].find(|x: char| x.is_ascii_alphabetic()) {
+                Some(alpha_index) => {
+                    let next_alpha = self.off + alpha_index;
+                    let ret = &self.s[self.off..=next_alpha];
+                    self.off = next_alpha + 1;
+
+                    if &self.s[next_alpha..next_alpha + 1] == "m" {
+                        match AnsiColor::from_str(ret) {
+                            Ok(c) => Escaped(EscapedCode::Color(c)),
+                            Err(_) => Str(ret),
+                        }
+                    } else {
+                        match CursorMove::from_str(ret) {
+                            Ok(c) => Escaped(EscapedCode::CursorMove(c)),
+                            Err(_) => Str(ret),
+                        }
+                    }
                 }
-            } else {
-                match CursorMove::from_str(ret) {
-                    Ok(c) => Escaped(EscapedCode::CursorMove(c)),
-                    Err(_) => Str(ret),
+                None => {
+                    self.off = self.s.len();
+                    Str(self.s)
                 }
             }
         } else {
-            assert_str_is_valid(&self.s[self.off..]);
             let next_escape = self.off
                 + self.s[self.off..]
                     .find(|x: char| x == 0x1b as char)
                     .unwrap_or(self.s[self.off..].len());
-            assert!(next_escape <= self.s.len());
             let ret = &self.s[self.off..next_escape];
-            assert_str_is_valid(ret);
             self.off = next_escape;
             Str(ret)
         })
