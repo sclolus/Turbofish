5c138018591c03b14651b24ad403c85c3b04f22b
miniske <miniske@Debian-Seb-PC.Debian-Seb-PC>
2019-10-04 13:12:54 +0200

    Silenced the DUMMY invocations

--- libc/include/custom.h
+++ libc/include/custom.h
@@ -5,8 +5,11 @@
 #include <unistd.h>
 #include <stdbool.h>
 
-#define DUMMY dprintf(STDERR_FILENO, "dummy function called: %s\n", __func__);
-#define DUMMY_KERNEL dprintf(STDERR_FILENO, "dummy 'Kernel' implementation called: %s\n", __func__);
+/* #define DUMMY dprintf(STDERR_FILENO, "dummy function called: %s\n", __func__); */
+/* #define DUMMY_KERNEL dprintf(STDERR_FILENO, "dummy 'Kernel' implementation called: %s\n", __func__); */
+
+#define DUMMY do {} while (0);
+#define DUMMY_KERNEL do {} while (0);
 
 void exit_qemu(int status);
 bool is_ptr_valid(const char *path);
