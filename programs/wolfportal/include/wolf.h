294f5c7c57c624748e7430fc52e96cf6f2886ca1
mordak <bmickael@student.42.fr>
2019-10-08 19:33:58 +0000

    Fix wolfs mouvements

--- programs/wolfportal/include/wolf.h
+++ programs/wolfportal/include/wolf.h
@@ -114,17 +114,38 @@ typedef struct		s_key
 # define BUTTON3 3
 # define BUTTON4 4
 # define BUTTON5 5
-# define KEY_LEFT 123
-# define KEY_DOWN 125
-# define KEY_RIGHT 124
-# define KEY_UP 126
-# define KEY_B 48
+
+# define KEY_LEFT 65361
+# define KEY_DOWN 65364
+# define KEY_RIGHT 65363
+# define KEY_UP 65362
+# define KEY_B 11
 # define KEY_PAD_SUB 78
 # define KEY_PAD_ADD 69
-# define KEY_R 19
-# define KEY_Z 44
-# define KEY_H 35
-# define KEY_S 31
+# define KEY_R 15
+# define KEY_Z 122
+# define KEY_H 4
+# define KEY_S 115
+
+/*
+-# define KEY_LEFT 123
+-# define KEY_DOWN 125
+-# define KEY_RIGHT 124
+-# define KEY_UP 126
++# define KEY_LEFT 65361
++# define KEY_DOWN 65364
++# define KEY_RIGHT 65363
++# define KEY_UP 65362
+ # define KEY_B 11
+ # define KEY_PAD_SUB 78
+ # define KEY_PAD_ADD 69
+ # define KEY_R 15
+-# define KEY_Z 6
++# define KEY_Z 122
+ # define KEY_H 4
+-# define KEY_S 1
++# define KEY_S 115
+*/
 
 void				ft_parse_input(char *name);
 void				add_start_position(int i, int j);
